0d98da44226e1a380d11bbd0df25b01cdc0d0a19
95度茅台# <dj4qiao@sina.com>
2022-09-20 22:46:20 +0800

    Edit text

--- vip/showImage.js
+++ vip/showImage.js
@@ -1,27 +1,27 @@
-let widget = await createWidget();
+const widget = await createWidget();
 
   if (!config.runsInWidget) {
-    await widget.presentLarge();
-  }
-  Script.setWidget(widget);
+    await widget.presentLarge();  
+  }  
+  Script.setWidget(widget);  
   Script.complete();
   
   async function createWidget() {
-    let widget = new ListWidget();
-    let bg = await getImage('https://gitcode.net/4qiao/shortcuts/raw/master/img/IMG_9332.png');
+    const widget = new ListWidget();
+    const bg = await getImage('https://gitcode.net/4qiao/shortcuts/raw/master/update/img/Lamborghini.jpeg');
     widget.backgroundImage = await shadowImage(bg);
-    return widget;
+    return widget;  
   }
-  
-  async function getImage(url) {
-    let r = await new Request(url);
+
+   async function getImage(url) {
+    const r = await new Request(url);
     return await r.loadImage();
-  }
+   }
   
   async function shadowImage(img) {
-    let ctx = new DrawContext();
+    const ctx = new DrawContext();
     ctx.size = img.size;
     ctx.drawImageInRect(img, new Rect(0, 0, img.size['width'], img.size['height']));
-    let res = await ctx.getImage();
-    return res;
+    const res = await ctx.getImage();
+    return res;  
   }
