623e042c495802e6ef8d172a09ff0c74cb344373
95度茅台# <dj4qiao@sina.com>
2022-09-21 22:20:11 +0800

    upload text

---
+++ api/aaa.js
@@ -0,0 +1,13 @@
+const FILE_MGR = FileManager[module.filename.includes('Documents/iCloud~') ? 'iCloud' : 'local']();
+await Promise.all(
+    ['「小件件」路虎出行.js'].map(async (js) => {
+        const REQ = new Request('https://api.ningkai.wang/scriptable-scripts/landrover/landrover.js');
+        const RES = await REQ.load();
+        FILE_MGR.write(FILE_MGR.joinPath(FILE_MGR.documentsDirectory(), js), RES);
+    })
+);
+FILE_MGR.remove(module.filename);
+Safari.open(
+    'scriptable:///open?scriptName=' + encodeURIComponent('「小件件」路虎出行')
+);
+  
