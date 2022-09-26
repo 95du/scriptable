f840e9940679d03ada672e8a7994ef5c44800808
95度茅台# <dj4qiao@sina.com>
2022-09-26 18:25:56 +0800

    Edit text

--- api/hondaCivic.js
+++ api/hondaCivic.js
@@ -5,7 +5,7 @@ const widget = await createWidget();
     const widget = new ListWidget();
     const bg = await getImage('https://gitcode.net/4qiao/shortcuts/raw/master/update/img/Maybach.png');
     widget.backgroundImage = await shadowImage(bg);
-    return widget;  
+    return widget;
   }
 
   async function getImage(url) {
@@ -20,12 +20,8 @@ const widget = await createWidget();
     const res = await ctx.getImage();
     return res;  
   }
-  
-  //刷新widget（间隔5分钟）,官方服务有请求次数限制（50次/h）,根据个人喜好修改最后一位数字（改成几就是间隔几分钟刷新）
-  const interval = 1000 * 60 * 60;
-   widget.refreshAfterDate = new Date(Date.now() + interval);
-  
-  
+    
+    
 //coding cookie
 const cookie = ('code=artifact-reforge%3Dfalse%2Casync-blocked%3Dtrue%2Cauth-by-wechat%3Dtrue%2Cci-qci%3Dfalse%2Cci-team-step%3Dfalse%2Cci-team-templates%3Dfalse%2Ccoding-flow%3Dfalse%2Ccoding-ocd-java%3Dfalse%2Ccoding-ocd-pages%3Dtrue%2Centerprise-permission-management%3Dtrue%2Cmobile-layout-test%3Dfalse%2Cproject-permission-management%3Dtrue%2Cservice-exception-tips%3Dfalse%2Ctencent-cloud-object-storage%3Dtrue%2C5b585a51; _ga=GA1.2.1553488068.1664098682; _gid=GA1.2.292291750.1664098682; enterprise_domain=diqiao; eid=8498be9b-b0b9-4575-be7b-609054e63564; c=auth-by-wechat%3Dtrue%2Cproject-permission-management%3Dtrue%2Centerprise-permission-management%3Dtrue%2C5c58505d; exp=89cd78c2; ac=9543735c-c43a-4a9a-8962-fdd4eaaadeba; login=4c0b000d-e6d1-4eee-b323-21ddaec6c513; XSRF-TOKEN=e6a5aade-0613-4c0f-8447-ed8415f80134')
 
@@ -58,12 +54,14 @@ const cookie = ('code=artifact-reforge%3Dfalse%2Casync-blocked%3Dtrue%2Cauth-by-
     "updateTime": "${data.updateTime}", 
     "address": "${address}", 
     "run": "${data.owner}", 
+    "coordinates": "${data.longitude},${data.latitude}",
     "time": "${timestamp}"
     }`
     
     object = `{
     "updateTime": "${data.updateTime}",        
     "address": "${address}", 
+    "run": "runningStop", 
     "coordinates": "${data.longitude},${data.latitude}",
     "time": "${timestamp}"
     }`
@@ -81,18 +79,18 @@ const cookie = ('code=artifact-reforge%3Dfalse%2Casync-blocked%3Dtrue%2Cauth-by-
         //speed
         if (data.speed <= 5) {
           obj = {
-            "Status": "车辆静止中",
+            "Status": "[ 车辆静止中 ]",
             "Position" : `https://maps.apple.com/?q=HONDA&ll=${data.latitude},${data.longitude}&t=m`
           };
         } else {
           obj = {
-            "Status": `车速 ${data.speed} km/h`,
+            "Status": `[ 车速 ] ${data.speed} km/h`,
             "Position" : `https://maps.apple.com/?q=HONDA&ll=${data.latitude},${data.longitude}&t=m`
           };
         }
     
     
-    //icon
+    //icon text
     const iconStack = widget.addStack();
     const iconSymbol = SFSymbol.named('paperplane.fill');
     const naviIcon = iconStack.addImage(iconSymbol.image);
@@ -100,62 +98,98 @@ const cookie = ('code=artifact-reforge%3Dfalse%2Casync-blocked%3Dtrue%2Cauth-by-
  naviIcon.tintColor = Color.blue();  
     iconStack.addSpacer(10);
     
-    const carText = iconStack.addText('Maybach 迈巴赫 680');
+    const carText = iconStack.addText('Mercedes Maybach 680');
     carText.textColor = Color.black();
     carText.font = Font.boldSystemFont(15);  
     widget.addSpacer(113);
-
     //Jump Map
     naviIcon.url = `${obj.Position}`;
  iconStack.useDefaultPadding();
+
+    if (!config.runsInWidget) {
+      await widget.presentMedium();  
+    }  
+    Script.setWidget(widget);  
+    Script.complete();
 
 
     //get token
     const Req = new Request('https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=ww1ce681aef2442dad&corpsecret=Oy7opWLXZimnS_s76YkuHexs12OrUOwYEoMxwLTaxX4');
     const Res = await Req.loadJSON();
       
-    //Path planning
-    const requ = new Request(`https://restapi.amap.com/v5/direction/driving?key=a35a9538433a183718ce973382012f55&origin_type=0&strategy=38&origin=${File.coordinates}&destination=${data.longitude},${data.latitude}`);  
-    resp = await requ.loadJSON();
-    const distance = resp.route.paths[0].distance  
-    if (distance > 20){
-     return;
+    /**
+    *Electronic Fence
+    *判断run没有值触发电子围栏
+    *推送到微信
+    */
+    if(File.run != 'HONDA'){
+      const fence = new Request(`https://restapi.amap.com/v5/direction/driving?key=a35a9538433a183718ce973382012f55&origin_type=0&strategy=38&origin=${File.coordinates}&destination=${data.longitude},${data.latitude}`);  
+      resFence = await fence.loadJSON();
+      const distance = resFence.route.paths[0].distance  
+    
+      if (distance > 20){
+      //push message to WeChat_1
+      const weChat_1 = new Request(`https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${Res.access_token}`);
+      weChat_1.method = 'POST'
+      weChat_1.body = `{"touser":"DianQiao","agentid":"1000004","msgtype":"news","news":{"articles":[{"title":"${address}","picurl":"https://restapi.amap.com/v3/staticmap?&key=a35a9538433a183718ce973382012f55&zoom=14&size=450*300&markers=-1,https://image.fosunholiday.com/cl/image/comment/619016bf24e0bc56ff2a968a_Locating_9.png,0:${data.longitude},${data.latitude}","description":"[ ${obj.Status} ]  已离开📍${File.address}（ 相距 ${distance} 米 ）\n更新时间 ${GMT}","url":"${obj.Position}"}]}}`;
+      const res_1 = await weChat_1.loadJSON();
+      
+      //Notification_1
+      notice.title = `${obj.Status}  `+`更新时间 ${GMT}`
+      notice.body = `已离开📍${File.address}（ 相距 ${distance} 米 ）\n更新时间 ${GMT}`
+      notice.openURL = `${obj.Position}`
+      notice.schedule()
+    
+      //edit file_1
+      const edit_1 = new Request('https://diqiao.coding.net/api/user/diqiao/project/shortcuts/depot/4qiao/git/blob/master/code/script.json')
+      edit_1.method = 'GET'
+      edit_1.headers = {"Cookie": `${cookie}`}
+      const Edit_1 = await edit_1.loadJSON();
+    
+      //upload JSON_1
+      const up_1 = new Request('https://diqiao.coding.net/api/user/diqiao/project/shortcuts/depot/4qiao/git/edit/master/code/script.json')
+      up_1.method = 'POST'
+      up_1.headers = {"Cookie": `${cookie}`,"X-XSRF-TOKEN": "e6a5aade-0613-4c0f-8447-ed8415f80134"}  
+      up_1.body = `newRef=&newPath=&message="upload"&content=${runObj}&lastCommitSha=${Edit_1.data.headCommit.commitId}`
+      const upload_1= await up_1.loadJSON();
+      return;//pushEnd_1
+      }
     }
       
-           
-    //push message to WeChat
-    const REq = new Request(`https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${Res.access_token}`);
-    REq.method = 'POST'
-    REq.body = `{"touser":"DianQiao","agentid":"1000004","msgtype":"news","news":{"articles":[{"title":"${address}","picurl":"https://restapi.amap.com/v3/staticmap?&key=a35a9538433a183718ce973382012f55&zoom=14&size=450*300&markers=-1,https://image.fosunholiday.com/cl/image/comment/619016bf24e0bc56ff2a968a_Locating_9.png,0:${data.longitude},${data.latitude}","description":"[ ${obj.Status} ]  更新时间 ${GMT}","url":"${obj.Position}"}]}}`;
-    const REs = await REq.loadJSON();
       
-    //Notification
-    notice.title = `${obj.Status}，`+`更新时间 ${GMT}`
-    notice.body = `${address}`
-    //notice.sound = 'alert'
-    notice.openURL = `${obj.Position}`
-    notice.schedule()
+    /**
+    *车辆状态触发条件
+    *驻车时长，行驶中，静止状态
+    *推送到微信
+    */
+    if (data.speed <= 5) {
+       //刷新widget（间隔5分钟）,官方服务有请求次数限制（50次/h）,根据个人喜好修改最后一位数字（改成几就是间隔几分钟刷新）
+       const interval = 1000 * 60 * 30;
+       widget.refreshAfterDate = new Date(Date.now() + interval);
+    }
+      //push message to WeChat_2
+      const weChat_2 = new Request(`https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${Res.access_token}`);
+      weChat_2.method = 'POST'
+      weChat_2.body = `{"touser":"DianQiao","agentid":"1000004","msgtype":"news","news":{"articles":[{"title":"${address}","picurl":"https://restapi.amap.com/v3/staticmap?&key=a35a9538433a183718ce973382012f55&zoom=14&size=450*300&markers=-1,https://image.fosunholiday.com/cl/image/comment/619016bf24e0bc56ff2a968a_Locating_9.png,0:${data.longitude},${data.latitude}","description":"[ ${obj.Status} ]  更新时间 ${GMT}","url":"${obj.Position}"}]}}`;
+      const res_2 = await weChat_2.loadJSON();
+      
+      //Notification_2
+      notice.title = `${obj.Status}  `+`更新时间 ${GMT}`
+      notice.body = `${address}`
+      notice.openURL = `${obj.Position}`
+      notice.schedule()
     
+      //edit file_2
+      const edit_2 = new Request('https://diqiao.coding.net/api/user/diqiao/project/shortcuts/depot/4qiao/git/blob/master/code/script.json')
+      edit_2.method = 'GET'
+      edit_2.headers = {"Cookie": `${cookie}`}
+      const Edit_2 = await edit_2.loadJSON();
     
-    
-    //edit file
-    const edit = new Request('https://diqiao.coding.net/api/user/diqiao/project/shortcuts/depot/4qiao/git/blob/master/code/script.json')
-    edit.method = 'GET'
-    edit.headers = {"Cookie": `${cookie}`}
-    const Edit = await edit.loadJSON();
-    
-    //upload JSON
-    const up = new Request('https://diqiao.coding.net/api/user/diqiao/project/shortcuts/depot/4qiao/git/edit/master/code/script.json')
-    up.method = 'POST'
-    up.headers = {"Cookie": `${cookie}`,"X-XSRF-TOKEN": "e6a5aade-0613-4c0f-8447-ed8415f80134"}  
-    up.body = `newRef=&newPath=&message="upload"&content=${object}&lastCommitSha=${Edit.data.headCommit.commitId}`
-    const upload = await up.loadJSON();
-    
-    
-    
-    if (!config.runsInWidget) {
-      await widget.presentMedium();  
-    }  
-    Script.setWidget(widget);  
-    Script.complete();
-   
+      //upload JSON_2
+      const up_2 = new Request('https://diqiao.coding.net/api/user/diqiao/project/shortcuts/depot/4qiao/git/edit/master/code/script.json')
+      up_2.method = 'POST'
+      up_2.headers = {"Cookie": `${cookie}`,"X-XSRF-TOKEN": "e6a5aade-0613-4c0f-8447-ed8415f80134"}  
+      up_2.body = `newRef=&newPath=&message="upload"&content=${object}&lastCommitSha=${Edit_2.data.headCommit.commitId}`
+      const upload_2 = await up_2.loadJSON();
+      return;//pushEnd_2
+  
