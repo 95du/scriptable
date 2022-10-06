826611ad10b880c8bfe85a2f56b6bd65c7e9abe7
95度茅台 <dj4qiao@sina.com>
2022-10-06 12:09:05 +0800

    Edit text

--- code/update.js
+++ code/update.js
@@ -1,20 +1,11 @@
-
-// This script was created by Max Zeryck.
-
-// The amount of blurring. Default is 150.
-let blur = 150
- 
-// Determine if user has taken the screenshot.
 var message
 message = "Before you start, go to your home screen and enter wiggle mode. Scroll to the empty page on the far right and take a screenshot."
-let options = ["查看小组件","退出选择","更新数据"]
+let options = ["查看小组件","更新数据","退出选择"]
 let response = await generateAlert(message,options)
 
-// Return if we need to exit.
-if (response == 1) return
 
 // Update the code.
-if (response == 2) {
+if (response === 1) {
   let files = FileManager.local()
   const iCloudInUse = files.isFileStoredIniCloud(module.filename)
 
@@ -25,27 +16,26 @@ if (response == 2) {
     const req = new Request("https://gitcode.net/4qiao/scriptable/raw/master/code/update.js")
     const codeString = await req.loadString()
     files.writeString(module.filename, codeString)
-    message = "The code has been updated. If the script is open, close it for the change to take effect."
+    message = "代码已更新。 如果脚本已打开，请将其关闭以使更改生效。"
   } catch {
-    message = "The update failed. Please try again later."
+    message = "更新失败，检查网络请稍后再试。"
   }
   options = ["OK"]
   await generateAlert(message,options)
   return
 }
 
+if (response === 2) return;
+
 Script.complete()
 
 // Generate an alert with the provided array of options.
 async function generateAlert(message,options) {
-  
   let alert = new Alert()
   alert.message = message
-  
   for (const option of options) {
     alert.addAction(option)
   }
-  
   let response = await alert.presentAlert()
   return response
 }
