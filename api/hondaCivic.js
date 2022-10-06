28660d1dde91e3f49385ffe7ec5da4a0f63b12a7
95度茅台 <dj4qiao@sina.com>
2022-10-06 21:44:44 +0800

    update script

--- api/hondaCivic.js
+++ api/hondaCivic.js
@@ -1,3 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: orange; icon-glyph: car;
 const notice = new Notification()
 const widget = await createWidget();
     
