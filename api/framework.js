30c6da569ad8607c7528e3b84b0e4eccada3c997
95度茅台 <dj4qiao@sina.com>
2022-10-10 18:49:53 +0800

    update script

--- api/framework.js
+++ api/framework.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: cyan; icon-glyph: cloud-download-alt;
+// icon-color: orange; icon-glyph: cloud-download-alt;
 const FILE_MGR = FileManager[module.filename.includes('Documents/iCloud~') ? 'iCloud' : 'local']();
 
 await Promise.all(
@@ -10,6 +10,6 @@ await Promise.all(
 FILE_MGR.write(FILE_MGR.joinPath(FILE_MGR.documentsDirectory(), js), RES);
   }));
 
-//FILE_MGR.remove(module.filename);
+FILE_MGR.remove(module.filename);
 
 Safari.open('scriptable:///run?scriptName=' + encodeURIComponent('framework.js'));
