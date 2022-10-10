186a052f884e941699021cb021ea368dc778fcdb
95度茅台 <dj4qiao@sina.com>
2022-10-10 18:45:01 +0800

    update script

--- api/framework.js
+++ api/framework.js
@@ -1,12 +1,15 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: orange; icon-glyph: cloud-download-alt;
-const req = new Request(`https://gitee.com/script_djg/scriptable/raw/master/Script/DJG.js`);
-const res = await req.loadString();
-const Name = 'Framework';
-  
-FileManager.local().writeString(FileManager.local().documentsDirectory() + `/${Name}.js`,`${res}`);
+// icon-color: cyan; icon-glyph: cloud-download-alt;
+const FILE_MGR = FileManager[module.filename.includes('Documents/iCloud~') ? 'iCloud' : 'local']();
+
+await Promise.all(
+  ['framework.js'].map(async (js) => {
+    const REQ = new Request('https://gitee.com/script_djg/scriptable/raw/master/Script/DJG.js');
+    const RES = await REQ.load();
+FILE_MGR.write(FILE_MGR.joinPath(FILE_MGR.documentsDirectory(), js), RES);
+  }));
 
 FILE_MGR.remove(module.filename);
 
- Safari.open('scriptable:///run/' + encodeURIComponent(Name));
+Safari.open('scriptable:///run?scriptName=' + encodeURIComponent('framework.js'));
