c1175f523bac8c1c4342715ed58937b8def11b9b
95度茅台 <dj4qiao@sina.com>
2022-10-13 23:08:39 +0800

    Delete install.js

--- api/install.js
+++
@@ -1,7 +0,0 @@
-const req = new Request(`https://gitcode.net/4qiao/scriptable/raw/master/api/bmw.js`);
-const res = await req.loadString();
-const Name = '正在安装小组件...';
-  
-FileManager.iCloud().writeString(FileManager.iCloud().documentsDirectory() + `/${Name}.js`,`${res}`);
-
- Safari.open('scriptable:///run/' + encodeURIComponent(Name));
