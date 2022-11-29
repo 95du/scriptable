dbfbe73fbb087a2cc36132698120ee8e15b63a36
95度茅台 <dj4qiao@sina.com>
2022-11-29 23:32:33 +0800

    upload new script

---
+++ api/bottomBar.js
@@ -0,0 +1,151 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-green; icon-glyph: comments;
+/**
+* iOS 16 负一屏底栏
+* 高仿微信通知信息样式
+* 内容显示未来两小时天气
+* 每日一句中英文
+* 自行替换第34行的背景URL
+* 小组件作者：95度茅台
+*/
+
+const stackBackground = Color.dynamic(
+  new Color('#EFEBE9', 0.6), 
+  new Color('#161D2A', 0.5)
+);
+const eventTextColor = Color.dynamic(
+  new Color('#1e1e1e'), 
+  new Color('#fefefe')
+);
+const stackSize = new Size(0, 70);
+
+
+try {
+  // Conversion GPS  
+  const location = await Location.current();
+  convert = await getJson(atob('aHR0cHM6Ly9yZXN0YXBpLmFtYXAuY29tL3YzL2Fzc2lzdGFudC9jb29yZGluYXRlL2NvbnZlcnQ/Y29vcmRzeXM9Z3BzJm91dHB1dD1qc29uJmtleT1hMzVhOTUzODQzM2ExODM3MThjZTk3MzM4MjAxMmY1NSZsb2NhdGlvbnM9') + `${location.longitude},${location.latitude}`);
+  widget = await createWidget();
+} catch(e) {
+  console.error(e);
+}
+
+
+async function createWidget() {
+  const widget = new ListWidget();
+  // Wechat icon
+  weChat = await getImage('https://gitcode.net/4qiao/scriptable/raw/master/img/icon/weChat.png');
+
+  // Background picture
+  const bg = await getImage(atob('aHR0cHM6Ly9zd2VpeGluZmlsZS5oaXNlbnNlLmNvbS9tZWRpYS9NMDAvNzEvRDAvQ2g0RnlHTl9vVC1BRWFhdUFBQmtUV21CQWc4MDguanBlZw=='));
+  widget.backgroundImage = bg
+  
+  // One word
+  const one = await getJson('http://open.iciba.com/dsapi');
+  
+  // Next two hours
+  await get({"url": "https://ssfc.api.moji.com/sfc/json/nowcast"})
+  
+  
+  /**
+  * Frame Layout
+  * Top Row Events
+  */
+  widget.setPadding(0, 0, 0, 0);
+  const eventStack = widget.addStack();
+  eventStack.setPadding(15, 15, 15, 15)
+  eventStack.layoutHorizontally();
+  eventStack.centerAlignContent();
+  eventStack.backgroundColor = stackBackground
+  eventStack.cornerRadius = 23
+  eventStack.size = stackSize
+  
+  // WeChat icon
+  const imageElement = eventStack.addImage(weChat);
+  imageElement.imageSize = new Size(42, 42);
+  eventStack.addSpacer(10);
+  
+  // Two Hours Weather
+  const twoHoursStack = eventStack.addStack();
+  twoHoursStack.layoutVertically();
+  twoHoursStack.centerAlignContent();
+  const weatherText = twoHoursStack.addText(result.radarData.title);
+  weatherText.font = Font.boldSystemFont(14);
+  weatherText.textColor = eventTextColor
+  weatherText.textOpacity = 0.9
+  
+  // Two Hours Weather
+  twoHoursStack.addSpacer(2);
+  twoHoursStack.layoutVertically();
+  const contentText = twoHoursStack.addText(result.radarData.content);
+  contentText.font = Font.boldSystemFont(13.5);
+  contentText.textColor = eventTextColor
+  contentText.textOpacity = 0.7
+  
+  // Right timeStack
+  eventStack.addSpacer();
+  const timeStack = eventStack.addStack();
+  timeStack.layoutVertically();
+  const statusText = timeStack.addText('现在');
+  statusText.font = Font.boldSystemFont(12);
+  statusText.textColor = eventTextColor
+  statusText.textOpacity = 0.4
+  timeStack.addSpacer();
+  widget.addSpacer(18)
+  
+  
+  /** 
+  * Bottom Content
+  * Alert image
+  * One word
+  */
+  const contentStack = widget.addStack();
+  contentStack.layoutHorizontally();
+  contentStack.centerAlignContent();
+  
+  contentStack.addSpacer();
+  contentStack.setPadding(0, 10, 0, 10);
+  const textElement = contentStack.addText(`${one.note}\n${one.content}`);
+  textElement.textColor = Color.white();
+  textElement.font = Font.boldSystemFont(15);
+  contentStack.addSpacer();
+  widget.addSpacer();
+  
+  if (config.runsInApp) {
+    await widget.presentMedium();
+  } else {
+    Script.setWidget(widget);
+    Script.complete();
+  }
+  
+  return widget
+}
+
+  
+async function get(opts) {
+  const coordinates = convert.locations.split(",");
+  const request = new Request(opts.url);
+  request.method = 'POST'
+  request.body = `{
+    "common": {
+      "platform": "iPhone",
+      "language": "CN"
+    }, 
+    "params": {
+      "lat": '${coordinates[1]}', 
+      "lon": '${coordinates[0]}'
+    }
+  }`
+  result = await request.loadJSON();
+  return result
+}
+
+async function getJson(url) {
+  const req = await new Request(url)
+  return await req.loadJSON();
+}
+
+async function getImage(url) {
+  const r = await new Request(url);
+  return await r.loadImage();
+}
