a167cde1354515428bc59088fe3efb4c3cd4ac3b
95度茅台 <dj4qiao@sina.com>
2022-12-07 12:39:54 +0800

    update script

--- vip/backgroundScript.js
+++ vip/backgroundScript.js
@@ -104,6 +104,10 @@ async function main() {
   const exportPhoto = await generateAlert(message, exportPhotoOptions);
   
   if (exportPhoto == 0) {
+    const uri = Script.name();  
+    const F_MGR = FileManager.local();
+    const folder = F_MGR.joinPath(F_MGR.documentsDirectory(), "bottomBar");
+    const bgImage = F_MGR.joinPath(folder, uri + ".jpg");
     F_MGR.writeImage(bgImage,imgCrop)
     Safari.open('scriptable:///run/' + encodeURIComponent(uri));
   } else if (exportPhoto == 1) {
