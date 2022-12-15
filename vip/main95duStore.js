941a21edbbcfed3946c5a4736165ecdbda7a1fb7
95度茅台 <dj4qiao@sina.com>
2022-12-15 18:07:49 +0800

    upload new script

---
+++ vip/main95duStore.js
@@ -0,0 +1,189 @@
+async function main() {
+  // Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: pink; icon-glyph: cloud-download-alt;
+async function main() {
+  const bgColor = Color.dynamic(
+    new Color('#F5F5F5'),
+    new Color('#000000')
+  );
+  const Files = FileManager.iCloud();
+  const RootPath = Files.documentsDirectory();
+  
+  const saveFileName = (fileName) => {
+    const hasSuffix = fileName.lastIndexOf(".") + 1;
+    return !hasSuffix ? `${fileName}.js` : fileName;
+  };
+  
+  const write = (fileName, content) => {
+    let file = saveFileName(fileName);
+    const filePath = Files.joinPath(RootPath, file);
+    Files.writeString(filePath, content);
+    return true;
+  };
+  
+  const saveFile = async ({ moduleName, url }) => {
+    const req = new Request(encodeURI(url));
+    const content = await req.loadString();
+    write(`${moduleName}`, content);
+    return true;
+  };
+  
+  const notify = async (title, body, url, opts = {}) => {
+    let n = new Notification();
+    n = Object.assign(n, opts);
+    n.title = title;
+    n.body = body;
+    if (url) n.openURL = url;
+    return await n.schedule();
+  };
+  
+  const renderTableList = async (data) => {
+    try {
+      const table = new UITable();
+      table.showSeparators = true;
+  
+      const gifRow = new UITableRow();
+      gifRow.height = 80 * Device.screenScale();
+      const gifImage = gifRow.addImageAtURL(atob('aHR0cHM6Ly9zd2VpeGluZmlsZS5oaXNlbnNlLmNvbS9tZWRpYS9NMDAvNzEvQzgvQ2g0RnlXT0k2b0NBZjRQMUFFZ0trSzZxVVVrNTQyLmdpZg=='));
+      gifImage.widthWeight = 0.4;
+      gifImage.centerAligned();
+      table.addRow(gifRow);
+  
+      // interval 1
+      const gapRow1 = new UITableRow();
+      gapRow1.height = 30;
+      gapRow1.backgroundColor = bgColor
+      table.addRow(gapRow1);
+  
+      // topRow
+      const topRow = new UITableRow();
+      topRow.height = 70;
+      const leftText = topRow.addButton('效果图');
+      leftText.widthWeight = 0.3;
+      leftText.onTap = async () => {
+        const webView = new WebView();
+        await webView.loadURL(atob('aHR0cHM6Ly9zd2VpeGluZmlsZS5oaXNlbnNlLmNvbS9tZWRpYS9NMDAvNzEvRjMvQ2g0RnlXT1NuM3FBVG9pUUFDT2ZoaVpaUzFJNzY4LnBuZw=='));
+        await webView.present(false);
+      };
+  
+      const authorImage = topRow.addImageAtURL('https://gitcode.net/4qiao/framework/raw/master/img/icon/4qiao.png');
+      authorImage.widthWeight = 0.4;
+      authorImage.centerAligned();
+  
+      const rightText = topRow.addButton('快捷指令');
+      rightText.widthWeight = 0.3;
+      rightText.rightAligned();
+      rightText.onTap = async () => {
+        Safari.openInApp('https://sharecuts.cn/user/KVlQooAqzA', false);
+      };
+      table.addRow(topRow);
+  
+      // interval 2
+      const gapRow2 = new UITableRow();
+      gapRow2.height = 30;
+      gapRow2.backgroundColor = bgColor
+      table.addRow(gapRow2);
+  
+      // 如果是节点，则先远程获取
+      const req = new Request(data.subscription);
+      const subscription = await req.loadJSON();
+      const apps = subscription.apps;
+      apps.forEach((item) => {
+        const r = new UITableRow();
+        r.height = 60;
+        const imgCell = UITableCell.imageAtURL(item.thumb);
+        imgCell.centerAligned();
+        r.addCell(imgCell);
+  
+        const nameCell = UITableCell.text(item.title);
+        nameCell.centerAligned();
+        r.addCell(nameCell);
+  
+        const downloadCell = UITableCell.button("获取");
+        downloadCell.centerAligned();
+        downloadCell.dismissOnTap = true;
+        downloadCell.onTap = async () => {
+          if (item.depend) {
+            try {
+              for (let i = 0; i < item.depend.length; i++) {
+                const relyItem = item.depend[i];
+                const _isWrite = await saveFile({
+                  moduleName: relyItem.name,
+                  url: relyItem.scriptURL,
+                });
+                if (_isWrite) {
+                  notify("下载提示", `依赖插件:${relyItem.name}下载/更新成功`);
+                }
+              }
+            } catch (e) {
+              console.log(e);
+            }
+          }
+          const isWrite = await saveFile({
+            moduleName: item.name,
+            url: item.scriptURL,
+          });
+          if (isWrite) {
+            notify("下载提示", `小组件:${item.title}下载/更新成功`);
+          }
+        };
+        r.addCell(downloadCell);
+        table.addRow(r);
+      });
+  
+      // interval 3
+      const gapRow3 = new UITableRow();
+      gapRow3.height = 30;
+      gapRow3.backgroundColor = bgColor
+      table.addRow(gapRow3);
+  
+      // videoRow
+      const videoRow = new UITableRow();
+      videoRow.height = 70;
+      const videoText = videoRow.addButton('Animusic HD Pipe Dreams Video');
+      videoText.widthWeight = 0.3;
+      videoText.centerAligned();
+      videoText.onTap = async () => {
+        await Safari.openInApp(atob('aHR0cHM6Ly9zd2VpeGluZmlsZS5oaXNlbnNlLmNvbS9tZWRpYS9NMDAvNzIvNUEvQ2g0RnlHT1l0dy1BSTI4Q0FPRDkzNDk1Y2hVMzMxLm1wNA=='), false);
+      };
+      table.addRow(videoRow);
+  
+      // bottom interval
+      const bottom = new UITableRow();
+      bottom.height = 225;
+      bottom.backgroundColor = bgColor
+      const bottomText = bottom.addText('Copyright ©️ 2022 界面修改自·@DmYY');
+      bottomText.widthWeight = 0.3;
+      bottomText.centerAligned();
+      bottomText.titleFont = Font.boldMonospacedSystemFont(10);
+      bottomText.titleColor = Color.gray();
+      table.addRow(bottom);
+      table.present(false);
+    } catch (e) {
+      console.log(e);
+      notify("错误提示", "订阅获取失败");
+    }
+  };
+  
+  const Run = async () => {
+    try {
+      await renderTableList({
+        author: '95度茅台',
+        subscription: 'https://gitcode.net/4qiao/framework/raw/master/scriptable/install.json'
+      });
+    } catch (e) {
+      console.log("缓存读取错误" + e);
+    }
+  };
+  await Run();
+}
+
+module.exports = {
+  main
+}
+}
+
+module.exports = {
+  main
+}
