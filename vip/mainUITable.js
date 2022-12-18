e5614ed745030e01f7d8b27b7e6112e028d870ab
95度茅台 <dj4qiao@sina.com>
2022-12-18 23:02:19 +0800

    update script

--- vip/mainUITable.js
+++ vip/mainUITable.js
@@ -1,3 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: red; icon-glyph: magic;
 async function main() {
   // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
@@ -322,9 +325,7 @@ async function preferences(table, arr, outfit) {
           options = ['完成'];
           await generateAlert(title, message, options);
         } else if (type == 'OS') {
-          Safari.openInApp(
-'https://developer.apple.com/news/releases', false
-          );
+          await abc()
         }
       }
     table.addRow(row);
