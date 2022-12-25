2ea02c9d147ff58ebf3983b45fa63bcf830a5b9a
95度茅台 <dj4qiao@sina.com>
2022-12-25 21:02:21 +0800

    update script

--- vip/mainScript12123.js
+++ vip/mainScript12123.js
@@ -1,106 +1,81 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: teal; icon-glyph: cloud-download-alt;
-/**
-支付宝小程序 交管12123
-小组件作者：95度茅台
-获取Token作者: @FoKit
-版本: Version 1.3
-Telegram 交流群 https://t.me/+ViT7uEUrIUV0B_iy
-
-获取Token重写：https://raw.githubusercontent.com/FoKit/Scripts/main/rewrite/get_12123_token.sgmodule
-使用方法：配置重写规则，手动运行小组件，按提示跳转到 支付宝12123小程序 登录即可自动抓取/更新Token。
-使用前，请确保您的代理APP已配置好BoxJs重写，BoxJs配置方法：https://chavyleung.gitbook.io/boxjs/
-
-一键添加 boxjs 重写到 Quantumult-X https://api.boxjs.app/quanx-install
-
-Boxjs订阅（可选）：http://boxjs.com/#/sub/add/https%3A%2F%2Fraw.githubusercontent.com%2FFoKit%2FScripts%2Fmain%2Fboxjs%2Ffokit.boxjs.json
-
-手动配置重写规则：
-=========Quantumult-X=========
-[rewrite_local]
-^https:\/\/miniappcsfw\.122\.gov\.cn:8443\/openapi\/invokeApi\/business\/biz url script-request-body https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_12123_token.js
-
-[MITM]
-hostname = miniappcsfw.122.gov.cn
-============Surge=============
-[Script]
-12123_Token = type=http-request,pattern=^https:\/\/miniappcsfw\.122\.gov\.cn:8443\/openapi\/invokeApi\/business\/biz,requires-body=1,max-size=0,timeout=1000,script-path=https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/get_12123_token.js,script-update-interval=0
-
-[MITM]
-hostname = %APPEND% miniappcsfw.122.gov.cn
-*/
-
+// icon-color: pink; icon-glyph: cloud-download-alt;
 async function main() {
-  const notice = new Notification()
-  const apiData = new Request(atob(
-  'aHR0cHM6Ly9naXRjb2RlLm5ldC80cWlhby9zaG9ydGN1dHMvcmF3L21hc3Rlci9hcGkvdXBkYXRlL3Zpb2xhdGlvbi5qc29u'));
-  const get = await apiData.loadJSON();
+  const get = await new Request(atob(
+'aHR0cHM6Ly9naXRjb2RlLm5ldC80cWlhby9zaG9ydGN1dHMvcmF3L21hc3Rlci9hcGkvdXBkYXRlL3Zpb2xhdGlvbi5qc29u')).loadJSON()
   const url = get.infoURL
   
-  const fileManager = FileManager.iCloud();
-  const folder = fileManager.joinPath(fileManager.documentsDirectory(), "violation");
-  const cacheFile = fileManager.joinPath(folder, 'data.json');
+  const uri = Script.name()
+  const F_MGR = FileManager.local();
+  const folder = F_MGR.joinPath(F_MGR.documentsDirectory(), "violation");
+  const cacheFile = F_MGR.joinPath(folder, 'data.json');
   
-  if (fileManager.fileExists(cacheFile)) {
-    data = fileManager.readString(cacheFile)
+  if (F_MGR.fileExists(cacheFile)) {
+    data = F_MGR.readString(cacheFile)
     data = JSON.parse(data);
     verifyToken = data.verifyToken
     myPlate = data.myPlate
   }
   
   
-  if (!fileManager.fileExists(folder) || verifyToken === undefined) {
-    // boxjs_data
-    boxjs_request = new Request('http://boxjs.com/query/data/token_12123');
-    boxjs_data = await boxjs_request.loadJSON();
-    verifyToken = boxjs_data.val
-    if (fileManager.fileExists(cacheFile)) {
-      data = {"verifyToken": `${verifyToken}`,"myPlate": `${myPlate}`}
-      data = JSON.stringify(data);
-      fileManager.writeString(cacheFile, data);
+  if (!F_MGR.fileExists(folder) || verifyToken === undefined) {
+    try {
+      boxjs_data = await new Request('http://boxjs.com/query/data/token_12123').loadJSON();
+      verifyToken = boxjs_data.val
+    } catch(e) {
+      if (config.runsInApp) {
+        Safari.open('quantumult-x://');
+        notify('获取boxJs数据失败 ⚠️', '需打开Quantumult-X获取verifyToken');
+      }
+    }
+    if (F_MGR.fileExists(cacheFile)) {
+      data = {
+        verifyToken: verifyToken,
+        myPlate: myPlate
+      }
+      F_MGR.writeString(cacheFile, JSON.stringify(data));
     }
   }
   
   
-  if (!fileManager.fileExists(cacheFile)) {
+  if (!F_MGR.fileExists(cacheFile)) {
     if (!verifyToken) {
       const loginAlert = new Alert();
       loginAlert.title = '交管 12123';
-      loginAlert.message = `\r\n注 : 自动获取Token需要Quantumult-X / Surge 辅助运行\n\n具体方法请查看小组件代码开头注释\n\r\n小组件作者: 95度茅台\n获取Token作者: @FoKit`;
+      loginAlert.message = `\r\n注 : 自动获取Token需要Quantumult-X / Surge 辅助运行，具体方法请查看小组件代码开头注释\n\r\n小组件作者: 95度茅台\n获取Token作者: @FoKit`;
       loginAlert.addAction('获取Token');
       loginAlert.addCancelAction('取消');
       login = await loginAlert.presentAlert();
-      if (login === -1) {
-        return;
-      } else {
-        Safari.open(`${get.alipay}`);
-        return;
-      }
+      if (login === -1) return;
+      Safari.open(get.alipay);
+      return;
     } else {
-      console.log(`boxjs_token 获取成功: ${boxjs_data.val}`);
-      const alert = new Alert();
-      alert.title = '输入车牌号';
-      alert.message = '将显示在小组件左上角'
-      alert.addTextField('输入车牌号');
-      alert.addAction('确定');
-      alert.addCancelAction('取消');
-      const input = await alert.presentAlert();
-      const value = alert.textFieldValue(0);
-      myPlate = value
-      
-      if (input === 0) {
-        if (!fileManager.fileExists(folder)) {fileManager.createDirectory(folder)}
-        data = {"verifyToken": `${boxjs_data.val}`,"myPlate": `${myPlate}`}
-        data = JSON.stringify(data);
-        fileManager.writeString(cacheFile, data);
-        notice.title = '登录成功'
-        notice.body = '请前往桌面添加中号小组件'
-        notice.schedule();
-      } else {
-        return;
-      }
+      notify('交管12123', `boxjs_token 获取成功: ${boxjs_data.val}`);
+      await addLicensePlate();
     }
+  }
+  
+  
+  async function addLicensePlate() {
+    const alert = new Alert();
+    alert.title = '输入车牌号';
+    alert.message = '将显示在小组件左上角'
+    alert.addTextField('输入车牌号', F_MGR.fileExists(cacheFile) ? myPlate : '');
+    alert.addAction('确定');
+    alert.addCancelAction('取消');
+    const input = await alert.presentAlert();
+    myPlate = alert.textFieldValue(0);
+    if (input === -1) return;
+      data = {
+        verifyToken: verifyToken,
+        myPlate: myPlate
+      }
+    if (!F_MGR.fileExists(folder)) {
+      F_MGR.createDirectory(folder)
+    }
+    F_MGR.writeString(cacheFile, JSON.stringify(data));
+    notify(myPlate, '您的车牌设置成功');
   }
   
   
@@ -108,21 +83,20 @@ async function main() {
   const violation = new Request(url);
   violation.method = 'POST'
   violation.body = `params={
-      "productId": "${get.productId}",
-      "api": "${get.api1}",
-      "version": "${get.version}",
-      "verifyToken": "${verifyToken}"
+    "productId": "${get.productId}",
+    "api": "${get.api1}",
+    "version": "${get.version}",
+    "verifyToken": "${verifyToken}"
   }`
   const main = await violation.loadJSON();
   const success = main.success
   
   
   if (success === true) {
-    var list = main.data.list[0]
-    if (list === undefined) {
-      console.log(
-        JSON.stringify(main, null, 2)
-      );
+    const list = main.data.list[0];
+    nothing = list === undefined;
+    if (nothing) {
+      console.log(main.resultMsg)
     } else {
       // issueOrganization plate
       const plate = list.plateNumber
@@ -181,17 +155,11 @@ async function main() {
     }
   } else {
     if (main.resultCode === 'SYSTEM_ERROR') {
-    notice.title = main.resultMsg
-      notice.schedule();
+    notify(main.resultMsg, '');
     } else {
-      data = {"myPlate": `${myPlate}`}
-      data = JSON.stringify(data);
-      fileManager.writeString(cacheFile, data);
-      // notice
-      notice.title = 'Token已过期 ⚠️'
-      notice.body = '点击通知框自动跳转到支付宝12123小程序页面获取最新的Token ( 请确保已打开辅助工具 )'
-      notice.openURL = get.alipay
-      notice.schedule();
+      data = { myPlate: myPlate }
+      F_MGR.writeString(cacheFile, JSON.stringify(data));
+      notify('Token已过期 ⚠️', '点击通知框自动跳转到支付宝12123小程序页面重新获取 ( 请确保已打开辅助工具 )', get.alipay);
     }
     return;
   }
@@ -202,74 +170,87 @@ async function main() {
     let alert = new Alert();
     alert.title = "交管 12123"
     alert.message = get.Ver
-    alert.addDestructiveAction('更新代码')
-    alert.addAction('GetToken')
-    alert.addAction('预览组件')
-    alert.addAction('退出')
+    alert.addDestructiveAction('更新代码');
+    alert.addDestructiveAction('重置所有');
+    alert.addAction('组件下载');
+    alert.addAction('修改车牌')
+    alert.addAction('预览组件');
+    alert.addAction('退出菜单');
     response = await alert.presentAlert();
     // menu action 1
     if (response === 1) {
-      Safari.open(get.alipay);
+      if (F_MGR.fileExists(folder)) {
+        await F_MGR.remove(folder);
+        Safari.open('scriptable:///run/' + encodeURIComponent(uri));
+      }
       return;
     }
     if (response === 2) {
+      const modulePath = await downloadModule();
+      if (modulePath != null) {
+        const importedModule = importModule(modulePath);
+        await importedModule.main();
+      }
+    }
+    if (response === 3) {
+      await addLicensePlate();
+    }
+    if (response === 4) {
       const widget = await createWidget(main);
       await widget.presentMedium();
     }
-    if (response === 3) return;
+    if (response === 5) return;
     if (response === 0) {
-      const FILE_MGR = FileManager.local();
-      const iCloudInUse = FILE_MGR.isFileStoredIniCloud(module.filename);
-      const reqUpdate = new Request(get.update);
-      const codeString = await reqUpdate.loadString();
+      const codeString = await new Request(get.update).loadString();
       const finish = new Alert();
       if (codeString.indexOf("交管12123") == -1) {
         finish.title = "更新失败"
-        finish.addAction('OK')
+        finish.addAction('OK');
         await finish.presentAlert();
       } else {
-        FILE_MGR.writeString(module.filename, codeString)
+        F_MGR.writeString(  
+          module.filename,
+          codeString
+        );
         finish.title = "更新成功"
-        finish.addAction('OK')
+        finish.addAction('OK');
         await finish.presentAlert();
-        const Name = 'violation';
-        Safari.open('scriptable:///run/' + encodeURIComponent(Name));
+        Safari.open('scriptable:///run/' + encodeURIComponent(uri));
       }
     }
   }
-    
-    
-  // config widget
-  if (config.widgetFamily === "small") {
-    return;
+  
+  const isMediumWidget =  config.widgetFamily === 'medium'
+  if (!config.runsInWidget) {
+    await presentMenu();
   } else {
-    if (!config.runsInWidget) {  
-      await presentMenu()
-    } else {
+    if (isMediumWidget) {
       const widget = await createWidget(main);
       Script.setWidget(widget);
       Script.complete();
+    } else {
+      await createErrorWidget();
     }
   }
-    
+  
   
   // createWidget
   async function createWidget() {
     const widget = new ListWidget();
     widget.backgroundColor = Color.white();
-    const gradient = new LinearGradient()
+    const gradient = new LinearGradient();
     color = [
-    "#82B1FF", 
-    "#757575", 
-    "#4FC3F7",
-    "#66CCFF",
-    "#99CCCC",
-    "#BCBBBB"
+      "#82B1FF",
+      "#757575",
+      "#4FC3F7",
+      "#66CCFF",
+      "#99CCCC",
+      "#BCBBBB"
     ]
     const items = color[Math.floor(Math.random()*color.length)];
     gradient.locations = [0, 1]
     gradient.colors = [
-      new Color(`${items}`, 0.5),
+      new Color(items, 0.5),
       new Color('#00000000')
     ]
     widget.backgroundGradient = gradient
@@ -287,9 +268,10 @@ async function main() {
     */
     const leftStack = mainStack.addStack();
     leftStack.layoutVertically();
+    leftStack.addSpacer();
     // plateStack
     const plateStack = leftStack.addStack();
-    textPlate = plateStack.addText(myPlate)
+    textPlate = plateStack.addText(myPlate);
     textPlate.font = Font.mediumSystemFont(19);
     textPlate.textColor = Color.black();
     leftStack.addSpacer(6)
@@ -301,28 +283,19 @@ async function main() {
     const man = SFSymbol.named('car');
     const carIcon = carIconStack.addImage(man.image);
     carIcon.imageSize = new Size(14, 14);
-    if (list === undefined) {
-      carIcon.tintColor = Color.black();
-    } else {
-      carIcon.tintColor = Color.red();
-    }
+    carIcon.tintColor = nothing ? Color.black() : Color.red();
     carIconStack.addSpacer(5);
     // vehicleModel
     const vehicleModel = carIconStack.addStack();
-    if (list === undefined) {
-      vehicleModelText = vehicleModel.addText('未处理违章 0');
-    } else {
-      vehicleModelText = vehicleModel.addText(`未处理违章 ${list.count} 条`);
-    }
+    vehicleModelText = vehicleModel.addText(nothing ? '未处理违章 0' : `未处理违章 ${list.count} 条`);
     vehicleModelText.font = Font.mediumSystemFont(12);
     vehicleModelText.textColor = new Color('#494949');
     leftStack.addSpacer(3)
   
-  
     // violationPoint
     const vioPointStack = leftStack.addStack();
     const vioPoint = vioPointStack.addStack();
-    if (list !== undefined) {
+    if (!nothing) {
       vioPointText = vioPoint.addText(`罚款${vio.fine}元、` + `扣${vio.violationPoint}分`);
       vioPointText.font = Font.mediumSystemFont(12);
       vioPointText.textColor = new Color('#484848');
@@ -331,7 +304,7 @@ async function main() {
       
     // update icon
     const updateTimeStack = leftStack.addStack();
-    if (list === undefined) {
+    if (nothing) {
       const iconSymbol2 = SFSymbol.named('person.crop.circle');
       const carIcon2 = updateTimeStack.addImage(iconSymbol2.image);
       carIcon2.imageSize = new Size(14, 14);
@@ -341,17 +314,10 @@ async function main() {
       
     // update time
     const updateTime = updateTimeStack.addStack();
-    if (list === undefined) {
-      textUpdateTime = updateTime.addText('Good Driving');
-      textUpdateTime.font = Font.mediumSystemFont(12);  
-      textUpdateTime.textColor = new Color('#484848');
-      leftStack.addSpacer(25)
-    } else {
-      textUpdateTime = updateTime.addText(`${vio.violationTime}`);
-      textUpdateTime.font = Font.mediumSystemFont(12);  
-      textUpdateTime.textColor = new Color('#484848');
-      leftStack.addSpacer(8)
-    }
+    const textUpdateTime = updateTime.addText(nothing ? 'Good Driving' : `${vio.violationTime}`);
+    textUpdateTime.font = Font.mediumSystemFont(12);  
+    textUpdateTime.textColor = new Color('#484848');
+    leftStack.addSpacer(nothing ? 25 : 8)
       
   
     // Status barRow
@@ -359,36 +325,24 @@ async function main() {
     barStack.layoutHorizontally();
     barStack.centerAlignContent();
     barStack.setPadding(3, 10, 3, 10);
-    if (list === undefined) {
-      // violation Early Warning
-      barStack.backgroundColor = new Color('#EEEEEE', 0.1);
-      barStack.cornerRadius = 10
-      barStack.borderColor = Color.green();
-      barStack.borderWidth = 2
-        
+    // violation Early Warning
+    barStack.backgroundColor = new Color('#EEEEEE', 0.1);
+    barStack.cornerRadius = 10
+    barStack.borderColor = nothing ? Color.green() : new Color('#FF1744', 0.7);
+    barStack.borderWidth = 2
+    if (nothing) {
       // bar icon
       const barIcon = SFSymbol.named('leaf.fill');
       const barIconElement = barStack.addImage(barIcon.image);
       barIconElement.imageSize = new Size(16, 16);
       barIconElement.tintColor = Color.green();
       barStack.addSpacer(4);
-      // bar text
-      const totalMonthBar = barStack.addText('无违章');
-      totalMonthBar.font = Font.mediumSystemFont(14);
-      totalMonthBar.textColor = new Color('#009201');
-      leftStack.addSpacer(8)
-    } else {
-      // Driver's license
-      barStack.backgroundColor = new Color('#EEEEEE', 0.1);
-      barStack.cornerRadius = 10
-      barStack.borderColor = new Color('#FF1744', 0.7);
-      barStack.borderWidth = 2
-      // bar text
-      const totalMonthBar = barStack.addText(`${vio.plateNumber}`);
-      totalMonthBar.font = Font.mediumSystemFont(14);
-      totalMonthBar.textColor = new Color('#D50000');
-      leftStack.addSpacer(8)
     }
+    // bar text
+    const totalMonthBar = barStack.addText(nothing ? '无违章' : `${vio.plateNumber}`);
+    totalMonthBar.font = Font.mediumSystemFont(14);
+    totalMonthBar.textColor = new Color(nothing ? '#009201' : '#D50000');
+    leftStack.addSpacer(8)
   
   
     // Driver's license bar
@@ -410,7 +364,7 @@ async function main() {
     const totalMonthBar2 = barStack2.addText('驾驶证');
     totalMonthBar2.font = Font.mediumSystemFont(14);
     totalMonthBar2.textColor = new Color('#757575');
-    leftStack.addSpacer(2)
+    leftStack.addSpacer();
   
   
     /*
@@ -421,9 +375,10 @@ async function main() {
     */
     const rightStack = mainStack.addStack();
     rightStack.layoutVertically();
+    rightStack.addSpacer();
     // Car Logo
     const carLogoStack = rightStack.addStack();
-    carLogoStack.addSpacer()
+    carLogoStack.addSpacer();
     textPlate2 = carLogoStack.addText('交管12123')
     textPlate2.font = Font.boldSystemFont(14);
     textPlate2.rightAlignText();
@@ -444,34 +399,59 @@ async function main() {
     tipsStack.layoutHorizontally();
     tipsStack.centerAlignContent();
     tipsStack.size = new Size(230, 30)
-    if (list === undefined) {
-      textAddress = tipsStack.addText('温馨提示: 请保持良好的驾驶习惯，务必遵守交通规则');
-    } else {
-      textAddress = tipsStack.addText(`${vio.violationAddress}，` + `${vio.violation}`)
-    }
+    const textAddress = tipsStack.addText(nothing ? '温馨提示: 请保持良好的驾驶习惯，务必遵守交通规则' : `${vio.violationAddress}，` + `${vio.violation}`);
     textAddress.font = Font.mediumSystemFont(11.3);
     textAddress.textColor = new Color('#484848');
     textAddress.centerAlignText();
+    rightStack.addSpacer();
   
     // jump show status
     barStack2.url = get.status;
     // jump to details
     textPlate2.url = get.details;
-    // jump 12123
-    widget.url = get.alipay;
     // jump show image
-    if (list !== undefined) {
-      textAddress.url = `${img}`;
+    if (!nothing) {
+      textAddress.url = img;
     }
     return widget;
   }
   
+  async function downloadModule() {
+    const modulePath = F_MGR.joinPath(folder, 'store.js');
+    if (F_MGR.fileExists(modulePath)) {
+      await F_MGR.remove(modulePath)
+    }
+    const req = new Request(atob('aHR0cHM6Ly9naXRjb2RlLm5ldC80cWlhby9zY3JpcHRhYmxlL3Jhdy9tYXN0ZXIvdmlwL21haW45NWR1U3RvcmUuanM='));
+    const moduleJs = await req.load().catch(() => {
+      return null;
+    });
+    if (moduleJs) {
+      F_MGR.write(modulePath, moduleJs);
+      return modulePath;
+    }
+  }
+  
+  async function notify (title, body, url, opts = {}) {
+    let n = new Notification()
+    n = Object.assign(n, opts);
+    n.title = title
+    n.body = body
+    if (url) n.openURL = url
+    return await n.schedule()
+  }
   
   async function getImage(url) {
     const r = await new Request(url);
     return await r.loadImage();
   }
   
+  async function createErrorWidget() {
+    const widget = new ListWidget();
+    const text = widget.addText('仅支持中尺寸');
+    text.font = Font.systemFont(17);
+    text.centerAlignText();
+    Script.setWidget(widget);
+  }
   
   async function shadowImage(img) {
     let ctx = new DrawContext()
@@ -480,11 +460,7 @@ async function main() {
     // 图片遮罩颜色、透明度设置
     ctx.setFillColor(new Color("#000000", 0.2))
     ctx.fillRect(new Rect(0, 0, img.size['width'], img.size['height']))
-    let res = await ctx.getImage()
-    return res
+    return await ctx.getImage()
   }
 }
-  
-module.exports = {
-  main
-}
+module.exports = { main }
