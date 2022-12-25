93a7d2d72fd1d5555e6b2381e67b1b3235fc8ab2
95度茅台 <dj4qiao@sina.com>
2022-12-26 00:11:24 +0800

    update script

--- vip/main95duStore.js
+++ vip/main95duStore.js
@@ -54,10 +54,7 @@ async function main() {
       table.addRow(topRow);
   
       // interval 1
-      const gapRow1 = new UITableRow();
-      gapRow1.height = 30;
-      gapRow1.backgroundColor = bgColor
-      table.addRow(gapRow1);
+      await gapRow(table);
   
       // 如果是节点，则先远程获取
       const subscription = await new Request(data.subscription).loadJSON()
@@ -89,10 +86,7 @@ async function main() {
       });
       
       // interval 2
-      const gapRow2 = new UITableRow();
-      gapRow2.height = 30;
-      gapRow2.backgroundColor = bgColor
-      table.addRow(gapRow2);
+      await gapRow(table);
       
       // videoRow
       const videoRow = new UITableRow();
@@ -121,6 +115,13 @@ async function main() {
       notify("错误提示", "订阅获取失败");
     }
   };
+  
+  async function gapRow(table) {
+    const gapRow = new UITableRow();
+    gapRow.height = 30;
+    gapRow.backgroundColor = bgColor
+    await table.addRow(gapRow);
+  }
   
   const Run = async () => {
     try {
