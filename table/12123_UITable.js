73dc122447aa8f436ff9c234fc57c6d6f16d0b5e
95度茅台 <dj4qiao@sina.com>
2023-01-04 03:05:04 +0800

    update script

--- table/12123_UITable.js
+++ table/12123_UITable.js
@@ -165,7 +165,7 @@ async function main() {
     const items = color[Math.floor(Math.random()*color.length)];
     gradient.locations = [0, 1]
     gradient.colors = [
-      new Color(items, setting.transparency),
+      new Color(items, 0.5),
       new Color('#00000000')
     ]
     widget.backgroundGradient = gradient
