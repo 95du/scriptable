0e84128facb6bfb34e4cee351befc73539c9f5c9
95度茅台 <dj4qiao@sina.com>
2023-01-04 06:41:08 +0800

    update script

--- table/oil_UITable.js
+++ table/oil_UITable.js
@@ -42,8 +42,8 @@ async function main() {
     widget = await createWidget(oil);
   }
   
-  const value = 6 - 1
-  const wide = 8 - 1
+  const value = 6
+  const wide = 8
   
   async function createWidget(oil) {
     // 组件背景渐变
