2e47d32b65e4f51d2dd266075ba4fb0eeab6d7da
95度茅台 <dj4qiao@sina.com>
2023-01-07 00:18:40 +0800

    upload new script

---
+++ boxjs/Script.js
@@ -0,0 +1,105 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: teal; icon-glyph: magic;
+<! DOCTYPE html>
+<html>
+<head>
+<title>托迈酷客-自动任务</title>
+   <meta charset="utf-8" />     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="https://nturpin0.github.io/OutputKit/bootstrap.min.css"  type="text/css">
+
+<style>
+body {
+  font-family: system-ui
+}
+a {
+  color: #007AFF
+}
+body {  
+  background:Gray !important
+}
+img {
+  max-width: 100%;
+  height: auto
+}
+</style>
+</head>
+<body>
+<div class="container rounded">
+<!–– OutputKit   Output Area ––>
+
+<div class="card bg-light text-dark" class="shadow p-3 mb-5 bg-white rounded" class="card border-0"> <div class="card-header"><h6><center><font color=#ECAA5E>复游会 亚特兰蒂斯<br /><font color=#2E9AFE>ThomasCook For you Club Atlantis</font></font></center></h6></div> <div class="card-body">
+<p><img src="https://is2-ssl.mzstatic.com/image/thumb/Purple126/v4/99/c2/b1/99c2b1c5-a3a6-4e8e-5a2f-67addabd79c2/source/512x512bb.png" alt="" /></p>
+<p><strong><font color=#E16969>托迈酷客App，微信、百度、支付宝小程序均可登录（ 可以先百度一下再使用此捷径 ）</font></strong></p>
+
+<hr />
+<table><thead><tr></tr></thead><tbody><tr></tr></tbody></table>
+<br />
+<h4><center><font color=#7E56BB>自动任务</font></center></h4>
+<ul>
+<li><strong>功能</strong> -- 自动完成 [ 托迈酷客 ] 每日签到、每日任务并领取积分以及各种免费或优惠的福利</li>
+<li><strong>积分</strong> -- 0元0邮费兑换商品，每个账号1年总积分大概有300多元的商品可兑换 </li>
+<li><strong>账号</strong> -- 支持多个账号同时进行任务。</li>
+</ul>
+<br />
+<h4><center><font color=#7E56BB>手自一体</font></center></h4>
+<ul>
+<li><strong>锁屏模式</strong> -- 创建自动化</li>
+<li><strong>后台模式</strong> -- 添加到主屏幕运行</li>
+</ul>
+<br />
+<h4><center><font color=#7E56BB>手机号登录</font></center></h4>
+<ul>
+<li><strong>Step 1</strong> -  输入手机号获取验证码。</li>
+<li><strong>Step 2</strong> - 填入验证码，即可使用。</li>
+<li><strong>Step 3</strong> - 指令自动获取Token，并保存在你的iCloud中。</li>
+<li><strong>作者95度茅台 <a href="https://jq.qq.com/?_wv=1027&k=VtrgK2iQ">点击此处反馈问题</a></strong></li>
+</ul>
+<br />
+<h4><center><font color=#7E56BB>支付宝登录</font></center></h4>
+<ul>
+<li><strong>Step 1</strong> -  当跳转到支付宝的托迈酷客页面后，点击授权登录</li>
+<li><strong>Step 2</strong> - 点击第二栏的全部权益</li>
+<li><strong>Step 3</strong> - 点击右上角的三点菜单键 - 分享 - 复制链接</li>
+<li><strong>Step 4</strong> - 复制链接后返回快捷指令登录</li>
+</ul>
+
+<hr />
+<table><thead><tr></tr></thead><tbody><tr></tr></tbody></table>
+<br />
+<h5><center><font color=#7E56BB>扫下方邀请码加入托迈酷客推荐官</font></center></h5>
+<p><img src="https://diqiao.coding.net/p/shortcuts/d/4qiao/git/raw/master/img/thomasCook/recommender_3.jpeg" alt="" /></p>
+<br />
+<h4><center><font color=#7E56BB>分享链接赚佣金</font></center></h4>
+<h5><center><font color=#E16969>👉 <a href="https://jq.qq.com/?_wv=1027&k=VtrgK2iQ">从快捷指令中进群了解详情</a> </font></center></h5>
+<p><img src="https://diqiao.coding.net/p/shortcuts/d/4qiao/git/raw/master/img/thomasCook/recommender_6.png" alt="" /></p>
+
+<hr />
+<p><strong><center>可在捷径中浏览1400多种<font color=#E16969>10.9元 - 1090积分</font>至<font color=#E16969>2299元 - 229900 积分</font>兑换的商品（ 其中包括数码家电、珠宝、美妆、生活用品等）</center></strong></p>
+<p><strong><center>登录完成后此页面将不再显示</center></strong></p>
+<p><center>Version : 1.0 - 2022.04.01<br />
+Shortcut Author : ４丶敲（ 原创 ）</center></p>
+
+<hr />
+<table><thead><tr></tr></thead><tbody><tr></tr><tr></tr></tbody></table><h5><center><font color=#2E9AFE>用积分兑换的商品示例</font></center></h5>
+<p><center><img src="https://foliday-b2b2c-bonus-pro.oss-cn-shanghai.aliyuncs.com/Image_goods/060CD91116094746A2D1CCA0909F8930.jpeg_400x400" alt="" /></center></p>
+<p><strong><center><font color=#E16969>36800 积分 👉 <a href="https://hotels.folidaymall.com/tc-fostay/retail/productDetail/6485?isSharefrom=tc_app&trackCode=43NVWO0666Y7">艾优智能暖风机</a></font></center></center></strong></p>
+
+<hr />
+<p><center><img src="https://foliday-b2b2c-bonus-pro.oss-cn-shanghai.aliyuncs.com/Image_goods/5CDC510F71524E9999490734F0C2E0DF.jpeg_400x400" alt="" /></center></p>
+<p><strong><center><font color=#E16969>19800 积分 👉 <a href="https://hotels.folidaymall.com/tc-fostay/retail/productDetail/6484?isSharefrom=tc_app&trackCode=43NVWO0666Y7">荷兰Apiyoo皮卡丘加湿器PY1</a></font></center></center></strong></p>
+
+<hr />
+<p><center><img src="https://foliday-b2b2c-bonus-pro.oss-cn-shanghai.aliyuncs.com/Image_goods/A816180539144FB2AD0DA697B362CBEC.jpeg_400x400" alt="" /></center></p>
+<p><strong><center><font color=#E16969>39900 积分 👉 <a href="https://hotels.folidaymall.com/tc-fostay/retail/productDetail/6483?isSharefrom=tc_app&trackCode=43NVWO0666Y7">日本GERM格沵无线电动研磨机</a></font></center></center></strong></p>
+
+<hr />
+<p><center><img src="https://foliday-b2b2c-bonus-pro.oss-cn-shanghai.aliyuncs.com/Image_goods/848F61D0247F459CA66DE333695FA2CA.jpeg_400x400" alt="" /></center></p>
+<p><strong><center><font color=#E16969>35900 积分 👉 <a href="https://hotels.folidaymall.com/tc-fostay/retail/productDetail/5133?isSharefrom=tc_app&trackCode=43NVWO0666Y7">【活式抽层 双层机身】英国 摩飞 MR6255 家用干果机烘干机风干机 4层</a></font></center></center></strong></p>
+
+<hr />
+<p><center><img src="https://foliday-b2b2c-bonus-pro.oss-cn-shanghai.aliyuncs.com/Image_goods/5959D5F3D5B34698A95D7AEEDBDF0419.jpeg_400x400" alt="" /></center></p>
+<p><strong><center><font color=#E16969>29900 积分 👉 <a href="https://hotels.folidaymall.com/tc-fostay/retail/productDetail/6226?isSharefrom=tc_app&trackCode=43NVWO0666Y7">迪士尼多功能电烤箱DI-B65255红色</a></font></center></center></strong></p>
+</div>
+</body>
+</html>
