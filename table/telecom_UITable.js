4162ad3ac7541411f2f6eb7fbeebb3e326a1ad51
95度茅台 <dj4qiao@sina.com>
2023-02-01 19:16:45 +0800

    update script

--- table/telecom_UITable.js
+++ table/telecom_UITable.js
@@ -71,6 +71,7 @@ async function main() {
   const flow = (bal / flowTotal * 100).toPrecision(3);
   
   const dayNumber = Math.floor(Date.now() / 1000 / 60 / 60 / 24);
+  console.log(setting)
   if (setting.init === 'false' || dayNumber !== setting.dayNumber) {
     setting = {
       ...setting,
