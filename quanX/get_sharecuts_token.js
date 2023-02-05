fb71aa42581e2c81e1943f9d6a706eee3104c932
95度茅台 <dj4qiao@sina.com>
2023-02-06 07:08:07 +0800

    update script

--- quanX/get_sharecuts_token.js
+++ quanX/get_sharecuts_token.js
@@ -30,8 +30,8 @@ $.is_debug = $.getdata('is_debug');
   function GetCookie() {
     if ($request && $request.url.indexOf("categories") > -1 && $request.headers) {
       debug($request.headers);
-      if ($request['headers']['X-Token'] !== $.token && $request['headers']['X-Token']) {
-        $.token = $request['headers']['X-Token'];
+      $.token = $request['headers']['X-Token'];
+      if ($request['headers']['X-Token'] !== $.token && $.token) {
         debug($.token);
         $.setdata($.token, $.token_key);
         // Get X-User
