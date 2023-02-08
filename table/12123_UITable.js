99a67028e24cbcc55754ed11f3746a30b46cc35b
95度茅台 <dj4qiao@sina.com>
2023-02-08 15:50:30 +0800

    update script

--- table/12123_UITable.js
+++ table/12123_UITable.js
@@ -190,7 +190,7 @@ async function main() {
      * @param {string} text
      * Cylindrical Bar Chart
      */
-    widget.setPadding(16, 18, 16, 16);
+    widget.setPadding(15, 18, 15, 15);
     widget.addSpacer();
     const mainStack = widget.addStack();
     mainStack.layoutHorizontally();
