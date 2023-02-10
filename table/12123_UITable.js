79d660b38a162e4d09e8d42cad588753500a7e5c
95度茅台 <dj4qiao@sina.com>
2023-02-10 21:05:11 +0800

    update script

--- table/12123_UITable.js
+++ table/12123_UITable.js
@@ -327,7 +327,7 @@ async function main() {
     textPlate2.font = Font.boldSystemFont(14);
     textPlate2.rightAlignText();
     textPlate2.textColor = new Color('#0061FF');
-    rightStack.addSpacer(nothing ? 16 : (vio.violationAddress + vio.violation).length <= 19 ? 17 : 14);
+    rightStack.addSpacer(16);
   
     // Car image
     const carImageStack = rightStack.addStack();
@@ -349,7 +349,7 @@ async function main() {
     tipsStack.centerAlignContent();
     tipsStack.size = new Size(Number(setting.layout), 30)
     const textAddress = tipsStack.addText(nothing ? '请保持良好的驾驶习惯，务必遵守交通规则' : `${vio.violationAddress}，` + `${vio.violation}`);
-    textAddress.font = Font.mediumSystemFont(nothing ? 11.5 : 11.3);
+    textAddress.font = Font.mediumSystemFont(nothing ? 11.5 : (vio.violationAddress + vio.violation).length <= 19 ? 11.5 : 11.3);
     textAddress.textColor = new Color('#484848');
     textAddress.centerAlignText();
     rightStack.addSpacer();
