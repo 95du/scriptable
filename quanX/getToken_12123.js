967f0a56e0d23a272bffc89b48e7a13d846870e0
95度茅台 <dj4qiao@sina.com>
2023-02-15 03:22:33 +0800

    update script

--- quanX/getToken_12123.js
+++ quanX/getToken_12123.js
@@ -40,7 +40,6 @@ $.is_debug = $.getdata('is_debug');
       debug($.rest_Body);
       $.rest_body = JSON.parse($.rest_Body);
       $.token = JSON.parse($.token);
-      debug($.token)
       if ($.rest_body.verifyToken !== $.token.verifyToken) {
         $.token = $.rest_Body
         debug($.token);
