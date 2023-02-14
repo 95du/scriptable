8651c99587d3a186b4896f7483d92ef83e264722
95度茅台 <dj4qiao@sina.com>
2023-02-15 00:21:04 +0800

    update script

--- quanX/getToken_12123.js
+++ quanX/getToken_12123.js
@@ -36,10 +36,10 @@ $.is_debug = $.getdata('is_debug');
   function GetCookie() {
     if ($request && $request.body && $request.body.indexOf("verifyToken") > -1) {
       debug($request.body);
-      $.rest_body = $request.body
+      $.rest_body = decodeURIComponent($request.body).replace("params=", "");
       debug($.rest_body);
       if ($.rest_body !== $.token) {
-        $.token = $.rest_body
+        $.token = $.rest_body;
         debug($.token);
         $.setdata($.token, $.token_key);
         $.msg($.name, ``, `12123_verifyToken 获取成功。`);
