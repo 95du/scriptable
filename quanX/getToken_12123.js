e7245e79957e22c3494252d1ea27213243d373a0
95度茅台 <dj4qiao@sina.com>
2023-02-15 01:52:25 +0800

    update script

--- quanX/getToken_12123.js
+++ quanX/getToken_12123.js
@@ -39,11 +39,11 @@ $.is_debug = $.getdata('is_debug');
       $.rest_body = decodeURIComponent($request.body).replace("params=", "");
       debug($.rest_body);
       $.rest_body = JSON.parse($.rest_body);
-      if ($.rest_body.verifyToken !== $.token.split("@")[0]) {
+      if ($.rest_body.verifyToken !== $.token.split(",")[0]) {
         $.token = $.rest_body.verifyToken;
         $.sign = $.rest_body.sign
         debug($.token);
-        $.setdata($.token + '@' + $.sign, $.token_key);
+        $.setdata($.token + ',' + $.sign, $.token_key);
         $.msg($.name, ``, `12123_verifyToken 获取成功。`);
         console.log(`12123_verifyToken获取成功:\n${$.token}`);
       } else {
