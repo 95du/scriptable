639f348253ebba9d76f0bf6e43244f3bfcd752ea
95度茅台 <dj4qiao@sina.com>
2023-02-18 23:44:16 +0800

    update script

--- table/12123_UITable.js
+++ table/12123_UITable.js
@@ -46,7 +46,7 @@ async function main() {
           referer: referer
         }
         F_MGR.writeString(cacheFile, JSON.stringify(data));  
-        notify('boxjs_12123', '参数获取成功，已储存到iCloud');
+        notify('boxjs_12123', '参数获取成功，重新运行/桌面组件刷新可见');
       }
     } catch(e) {
       notify('获取 boxjs 数据失败 ⚠️', '需打开 Quantumult-X 或其他辅助工具', 'quantumult-x://');
