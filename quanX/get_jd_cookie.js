62636fae9498f8face93857c055149d2de8c82ba
95度茅台 <dj4qiao@sina.com>
2023-02-20 12:12:00 +0800

    update script

--- quanX/get_jd_cookie.js
+++ quanX/get_jd_cookie.js
@@ -48,7 +48,7 @@ $.is_debug = $.getdata('is_debug');
   function GetCookie() {
     if ($request && $request.url.indexOf("https://homepage-gw.jd.com/datastream/") > -1 && $request.headers) {
       $.cookie = $request['headers']['Cookie'] || $request['headers']['cookie'];
-      $.jd_cookie = cookie.match(/pt_key=[^;]+;/)[0] + CookieV.match(/pt_pin=[^;]+;/)[0];
+      $.jd_cookie = cookie.match(/pt_key=[^;]+;/)[0] + cookie.match(/pt_pin=[^;]+;/)[0];
       if ($.jd_cookie !== $.boxjs_cookie) {
         $.setdata($.jd_cookie, $.cookie_key);
         $.msg($.name + '_Cookie 获取成功', ``, $.jd_cookie);
