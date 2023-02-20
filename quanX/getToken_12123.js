6dc79c7a6e82c58acaecaab549a5329335000a0b
95度茅台 <dj4qiao@sina.com>
2023-02-20 19:31:22 +0800

    update script

--- quanX/getToken_12123.js
+++ quanX/getToken_12123.js
@@ -29,9 +29,9 @@ $.is_debug = $.getdata('is_debug');
         $.setdata($.token + ',' + $.rest_body.sign + ',' + $.rest_body.authToken, $.body_key);
         if ($.rest_body.sign !== $.body.split(",")[1]) {
           $.msg($.name, ``, `12123_verifyToken/Sign 获取成功。`);
-          console.log(`12123_verifyToken/Sign 获取成功:\n${$.token}`);  
-          $done();
+          console.log(`12123_verifyToken/Sign 获取成功:\n${$.token}`);
         }
+        $done();
       } else {
         console.log(`verifyToken未变动，跳过更新🚫\n${$.token}`);
       }
