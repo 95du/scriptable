f1d9da82685d4917f18ef673c7b63bc835f3d182
95度茅台 <dj4qiao@sina.com>
2023-02-25 16:12:07 +0800

    update script

--- api/violation.js
+++ api/violation.js
@@ -394,7 +394,7 @@ async function createWidget() {
   // validPeriodEndDate
   const updateTime = dateStack.addStack();
   const textUpdateTime = updateTime.addText(nothing || !success || `${vio.violationTime}` === 'undefined' ? referer.match(/validPeriodEnd=(.+)&vehPhoneNumber/)[1] : `${vio.violationTime}`);
-  textUpdateTime.font = Font.mediumSystemFont(13);
+  textUpdateTime.font = Font.mediumSystemFont(nothing ? 13 : 12);
   textUpdateTime.textColor = new Color('#484848');
   leftStack.addSpacer(nothing || !success ? size.leftGap1 : size.leftGap2);
     
