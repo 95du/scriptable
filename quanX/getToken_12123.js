8edd60e414211a3190d7ed36c816e7a191815e14
95度茅台 <dj4qiao@sina.com>
2023-02-25 23:22:27 +0800

    update script

--- quanX/getToken_12123.js
+++ quanX/getToken_12123.js
@@ -27,8 +27,8 @@ $.is_debug = $.getdata('is_debug');
         if ($.token && $.rest_body.sign) {
           $.setdata($.token + ',' + $.rest_body.sign, $.body_key);
           if ($.token !== $.body.split(",")[0]) {
-            $.msg($.name, ``, `12123_verifyToken/Sign 获取成功。`);
-            console.log(`12123_verifyToken/Sign 获取成功:\n${$.token}`);
+            $.msg($.name, ``, `12123_verifyToken/Sign获取成功。`);
+            console.log(`12123_Sign/verifyToken获取成功:\n${$.token}`);
           }
         }
       } else {
