412f9f41a5df3963fbcec2b033bb89ba8c9d8dd9
95度茅台 <dj4qiao@sina.com>
2023-02-27 15:43:37 +0800

    update script

--- table/jingDong_UITable.js
+++ table/jingDong_UITable.js
@@ -97,7 +97,7 @@ async function main() {
     jdouIcon.imageSize = new Size(18, 18);
     jdNumStack.addSpacer(3);
     const contentText = jdNumStack.addText(info.jdNum.toString());
-    contentText.font = Font.boldSystemFont(16.5);
+    contentText.font = Font.boldSystemFont(16);
     contentText.textColor = jNumColor
     contentText.textOpacity = 0.7;
     topStack.addSpacer();
