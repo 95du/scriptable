d5e8d83a3f26511da5e48e4c5021100260f29038
95度茅台 <dj4qiao@sina.com>
2023-03-01 04:26:08 +0800

    update script

--- table/jingDong_UITable_bean.js
+++ table/jingDong_UITable_bean.js
@@ -138,17 +138,19 @@ const seventeen = df.string(new Date());
     contentStack.centerAlignContent()
     contentStack.addSpacer();
     contentStack.backgroundColor = stackBackground
-    contentStack.setPadding(10, 5, 10, 5);
+    contentStack.setPadding(10, -5, 10, 5);
     contentStack.cornerRadius = 23;
     contentStack.size = stackSize;
     // Logo image
     const logoStack = contentStack.addStack();
     const logoImage = await getImage('http://mtw.so/5ZaG1N');
     const logoIcon = logoStack.addImage(logoImage);
-    logoIcon.imageSize = new Size(45, 45);
+    logoIcon.imageSize = new Size(42, 42);
     contentStack.addSpacer(1);
     
     const weekStack = contentStack.addStack();
+    weekStack.layoutHorizontally();
+    weekStack.centerAlignContent();
     const threeStack = weekStack.addStack();
     threeStack.layoutVertically();
     threeStack.centerAlignContent();
@@ -160,11 +162,11 @@ const seventeen = df.string(new Date());
     threeStack.addSpacer(2);
     
     const barStack = weekStack.addStack();
-    barStack.setPadding(3, 8, 3, 8);
+    barStack.setPadding(3, 5, 3, 5);
     barStack.backgroundColor = new Color('#FF0000');
     barStack.cornerRadius = 10;
     const weekText = barStack.addText(sendBean.splitBeansDesc);
-    weekText.font = Font.mediumSystemFont(10);
+    weekText.font = Font.mediumSystemFont(8);
     weekText.textColor = Color.white();
     
     const billDate = threeStack.addText(`豆苗成长值 ${sendBean.growth}`);
