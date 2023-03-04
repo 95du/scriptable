4a640729a40f4cb4ec923d092e8c1c1b706ac479
95度茅台 <dj4qiao@sina.com>
2023-03-04 11:43:27 +0800

    update script

--- quanX/getToken_12123.js
+++ quanX/getToken_12123.js
@@ -24,8 +24,8 @@ $.is_debug = $.getdata('is_debug');
       $.rest_body = JSON.parse(decodeURIComponent($request.body).replace("params=", ""));  
       $.token = $.rest_body.verifyToken;  
       if ($.token && $.rest_body.sign) {
-      $.timestamp = Date.parse(new Date());
-        $.setdata($.token + ',' + $.rest_body.sign + ',' + $.timestamp, $.body_key);
+        $.timestamp = Date.parse(new Date());
+        $.setdata(`${$.token},${$.rest_body.sign},${$.timestamp}`, $.body_key);
         if ($.timestamp - $.body.split(",")[2] >= 5000 || $.token !== $.body.split(",")[0]) {
           $.msg($.name, ``, `12123_verifyToken/Sign获取成功。`);
           console.log(`12123_Sign/verifyToken获取成功:\n${$.token}`);
