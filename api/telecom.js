02945e0035885654cfaa01094095a724d0acf64b
95度茅台 <dj4qiao@sina.com>
2023-03-09 02:38:59 +0800

    update script

--- api/telecom.js
+++ api/telecom.js
@@ -100,7 +100,7 @@ if (!F_MGR.fileExists(cacheFile) || dayNumber !== setting.dayNumber) {
 }
 
 const Step1st = 25;
-const Step2nd = 83;
+const Step2nd = 85;
 const StepFin = 100;
 const barWidth = 15;
 const barHeigth = 105;
