93db21b6e4bb1a8918add85663d705100c59deec
95度茅台 <dj4qiao@sina.com>
2023-03-11 16:42:34 +0800

    update script

--- table/jingDongBill_UITable.js
+++ table/jingDongBill_UITable.js
@@ -214,7 +214,7 @@ async function main() {
     
     const state = asset.quota.state;
     const amount = state === '1' ? asset.bill.amount : '0.00';
-    const baitiaoText = btStack.addText(state === '1' && amount > '999.99' ? amount.toPrecision(5) : amount > '9999.99' ? Math.floor(amount) : amount);
+    const baitiaoText = btStack.addText(state === '1' && amount > '1000' ? Math.floor(amount) : amount);
     baitiaoText.font = Font.mediumSystemFont(14);
     leftStack.addSpacer(2);
     mainStack.addSpacer()
