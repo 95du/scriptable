d9213b48cfc2abfd06b5bed3e40a03779cb36e02
95度茅台 <dj4qiao@sina.com>
2023-03-18 23:50:27 +0800

    update script

--- table/jingDong_baitiao.js
+++ table/jingDong_baitiao.js
@@ -250,7 +250,7 @@ async function main() {
     * @param {string} string
     */
     const prgrWid = Number(setting.progressWidth);
-    const tempBarWidth = curScore === '0' ? prgrWid : prgrWid - 30;
+    const tempBarWidth = curScore == 0 ? prgrWid : curScore <= 100 ? prgrWid - 10 : (curScore > 100 && curScore <= 1000) ? prgrWid - 15 : (curScore > 1000 && curScore <= 10000) ? prgrWid - 25 : prgrWid - 32;
     const tempBarHeight = 18;
     const progressColor = "#f2f5f7"
     
