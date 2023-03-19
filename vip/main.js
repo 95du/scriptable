44d1ee70fda8df26235a1bee912da22ccabf7d52
95度茅台 <dj4qiao@sina.com>
2023-03-20 03:09:15 +0800

    Update text

--- vip/main.js
+++ vip/main.js
@@ -1,11 +1,5 @@
-async function main() {
-  // Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: deep-gray; icon-glyph: magic;
+// 95度茅台
 async function main() {
   ❤️
 }
-module.exports = { main }
-}
-// async function main()
 module.exports = { main }
