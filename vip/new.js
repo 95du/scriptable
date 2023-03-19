4edd285746c452d7983030f3f77f3d0029fa4a73
95度茅台 <dj4qiao@sina.com>
2023-03-20 02:57:53 +0800

    update script

--- vip/new.js
+++ vip/new.js
@@ -1,11 +1,7 @@
-async function main() {
-  // Variables used by Scriptable.
+// Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-gray; icon-glyph: magic;
 async function main() {
   ❤️
 }
-module.exports = { main }
-}
-// async function main()
 module.exports = { main }
