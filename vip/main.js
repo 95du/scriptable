13e9ecd6c15748f7ddf9fee61cd59527ee7bef77
95度茅台 <dj4qiao@sina.com>
2023-03-20 03:07:34 +0800

    upload new script

---
+++ vip/main.js
@@ -0,0 +1,11 @@
+async function main() {
+  // Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: magic;
+async function main() {
+  ❤️
+}
+module.exports = { main }
+}
+// async function main()
+module.exports = { main }
