b88e23a02b8f6360623901c859ef6864df49ab36
95度茅台 <dj4qiao@sina.com>
2023-03-24 02:32:44 +0800

    update script

--- api/maybach.js
+++ api/maybach.js
@@ -118,24 +118,31 @@ async function createWidget() {
   }
   const res = await req.loadJSON();
   if (res.code != 1) return;
-  const data = res.data
-  const mapUrl = `https://maps.apple.com/?q=HONDA&ll=${data.latitude},${data.longitude}&t=m`;
+  const {
+    speed,
+    owner,
+    longitude,
+    latitude,
+    updateTime
+  } = res.data
+  
+  const mapUrl = `https://maps.apple.com/?q=HONDA&ll=${latitude},${longitude}&t=m`;
 
-  if (data.speed <= 5) {
+  if (speed <= 5) {
     state = "已静止";
     status = "[ 车辆静止中 ]";
   } else {
-    state = `${data.speed} km·h`;
-    status = `[ 车速 ${data.speed} km·h ]`;
+    state = `${speed} km·h`;
+    status = `[ 车速 ${speed} km·h ]`;
   }
   
   // Get address (aMap)
-  const adr = await new Request(`http://restapi.amap.com/v3/geocode/regeo?key=9d6a1f278fdce6dd8873cd6f65cae2e0&s=rsv3&radius=500&extensions=all&location=${data.longitude},${data.latitude}`).loadJSON();
+  const adr = await new Request(`http://restapi.amap.com/v3/geocode/regeo?key=9d6a1f278fdce6dd8873cd6f65cae2e0&s=rsv3&radius=500&extensions=all&location=${longitude},${latitude}`).loadJSON();
   const address = adr.regeocode.formatted_address  
   
   // 计算停车时长(红绿灯图标)
   const timestamp = Date.parse(new Date());
-  const parkingTime = (timestamp - data.updateTime);
+  const parkingTime = (timestamp - updateTime);
   const days = Math.floor(parkingTime/(24 * 3600 * 1000));
   const P1 = parkingTime % (24 * 3600 * 1000);
   const hours1 = Math.floor(P1 / (3600 * 1000));
@@ -143,7 +150,7 @@ async function createWidget() {
   const minutes1 = Math.floor(P2 / (60 * 1000));
   
   // Timestamp Formatter
-  const date = new Date(data.updateTime);
+  const date = new Date(updateTime);
   const df = new DateFormatter();
   df.dateFormat = 'yyyy-MM-dd HH:mm';
   const GMT = (df.string(date));
@@ -152,10 +159,10 @@ async function createWidget() {
   
   // Saved Json
   runObj = {
-    updateTime: data.updateTime, 
+    updateTime: updateTime, 
     address: address,
-    run: data.owner,
-    coordinates: `${data.longitude},${data.latitude}`,
+    run: owner,
+    coordinates: `${longitude},${latitude}`,
     pushTime: timestamp,
     parkingTime: GMT2,
     cookie: json.cookie
@@ -163,7 +170,7 @@ async function createWidget() {
     
   object = {
     ...runObj,
-    run: data.speed
+    run: speed
   }
   // Initial Save
   if (!F_MGR.fileExists(cacheFile)) {
@@ -208,8 +215,7 @@ F_MGR.readString(cacheFile)
   carIcon1.imageSize = new Size(16, 16);
   benzStack.addSpacer(4);
   // mercedes text
-  const vehicleModel = benzStack.addStack();
-  const vehicleModelText = vehicleModel.addText('Mercedes');
+  const vehicleModelText = benzStack.addText('Mercedes');
   vehicleModelText.font = Font.mediumSystemFont(14);
   vehicleModelText.textColor = new Color('#424242');
   leftStack.addSpacer(3)
@@ -223,8 +229,7 @@ F_MGR.readString(cacheFile)
   carIcon2.imageSize = new Size(16, 16);
   dateStack.addSpacer(4);
   // update time text
-  const updateTime = dateStack.addStack();
-  const textUpdateTime = updateTime.addText(GMT2);
+  const textUpdateTime = dateStack.addText(GMT2);
   textUpdateTime.font = Font.mediumSystemFont(13);
   textUpdateTime.textColor = new Color('#424242');
   leftStack.addSpacer(22)
@@ -237,18 +242,18 @@ F_MGR.readString(cacheFile)
   
   barStack.backgroundColor = new Color('#EEEEEE', 0.1);
   barStack.cornerRadius = 10
-  barStack.borderColor = new Color(data.speed <= 5 ? '#AB47BC' : '#FF1744', 0.7);
+  barStack.borderColor = new Color(speed <= 5 ? '#AB47BC' : '#FF1744', 0.7);
   barStack.borderWidth = 2
   // bar icon
-  const barIcon = SFSymbol.named(data.speed <= 5 ? 'location' : 'location.fill');
+  const barIcon = SFSymbol.named(speed <= 5 ? 'location' : 'location.fill');
   const barIconElement = barStack.addImage(barIcon.image);
   barIconElement.imageSize = new Size(16, 16);
-  barIconElement.tintColor = data.speed <= 5 ? Color.purple() : Color.red();
+  barIconElement.tintColor = speed <= 5 ? Color.purple() : Color.red();
   barStack.addSpacer(4);
   // bar text
   const totalMonthBar = barStack.addText(state);
   totalMonthBar.font = Font.mediumSystemFont(14);
-  totalMonthBar.textColor = new Color(data.speed <= 5 ? '#AA00FF' : '#D50000');
+  totalMonthBar.textColor = new Color(speed <= 5 ? '#AA00FF' : '#D50000');
   leftStack.addSpacer(8)
 
   // Left Stack barRow2
@@ -299,14 +304,13 @@ F_MGR.readString(cacheFile)
   const item = resUrl.maybach[Math.floor(Math.random() * resUrl.maybach.length)];
   const carImage = await getImage(item);
   const imageCar = carImageStack.addImage(carImage);
-  imageCar.imageSize = new Size(225,100);
-  rightStack.addSpacer(1)
+  imageCar.imageSize = new Size(225, 100);
+  rightStack.addSpacer(1);
 
   // show address
   const adrStack = rightStack.addStack();
-  adrStack.layoutHorizontally();
   adrStack.centerAlignContent();
-  adrStack.size = new Size(226, 30)
+  adrStack.size = new Size(226, 30);
   const jmz = {};
   jmz.GetLength = function(str) {
     return str.replace(/[\u0391-\uFFE5]/g,"@@").length;
@@ -347,7 +351,7 @@ F_MGR.readString(cacheFile)
   const mapKey = atob('aHR0cHM6Ly9yZXN0YXBpLmFtYXAuY29tL3YzL3N0YXRpY21hcD8ma2V5PWEzNWE5NTM4NDMzYTE4MzcxOGNlOTczMzgyMDEyZjU1Jnpvb209MTQmc2l6ZT00NTAqMzAwJm1hcmtlcnM9LTEsaHR0cHM6Ly9pbWFnZS5mb3N1bmhvbGlkYXkuY29tL2NsL2ltYWdlL2NvbW1lbnQvNjE5MDE2YmYyNGUwYmM1NmZmMmE5NjhhX0xvY2F0aW5nXzkucG5n');
   
   if (json.run !== 'HONDA') {
-    const fence = await new Request(`https://restapi.amap.com/v5/direction/driving?key=a35a9538433a183718ce973382012f55&origin_type=0&strategy=38&origin=${json.coordinates}&destination=${data.longitude},${data.latitude}`).loadJSON();  
+    const fence = await new Request(`https://restapi.amap.com/v5/direction/driving?key=a35a9538433a183718ce973382012f55&origin_type=0&strategy=38&origin=${json.coordinates}&destination=${longitude},${latitude}`).loadJSON();  
     const distance = fence.route.paths[0].distance  
     
     if (distance > 20) {
@@ -362,7 +366,7 @@ F_MGR.readString(cacheFile)
           "articles": [
             {
               "title": "${address}",
-              "picurl": "${mapKey},0:${data.longitude},${data.latitude}",
+              "picurl": "${mapKey},0:${longitude},${latitude}",
               "description": "${status}  启动时间 ${GMT}\n已离开📍${json.address}，相距 ${distance} 米",
               "url": "${mapUrl}"
             }
@@ -394,8 +398,8 @@ F_MGR.readString(cacheFile)
   const seconds = Math.round(L3 / 1000);
   let moment = (hours * 60 + minutes)
   
-  if (data.speed <= 5) {
-    const duration = data.updateTime == json.updateTime ? 180 : 10
+  if (speed <= 5) {
+    const duration = updateTime == json.updateTime ? 180 : 10
     if (moment >= duration) {
       // push message to WeChat_2
       const weChat_2 = new Request(`https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${acc.access_token}`);
@@ -408,7 +412,7 @@ F_MGR.readString(cacheFile)
           "articles": [
             {
               "title": "${address}",
-              "picurl": "${mapKey},0:${data.longitude},${data.latitude}",
+              "picurl": "${mapKey},0:${longitude},${latitude}",
               "description": "${status} 停车时间 ${GMT}",
               "url": "${mapUrl}"
             }
@@ -435,7 +439,7 @@ F_MGR.readString(cacheFile)
           "articles": [
             {
               "title": "${address}",
-              "picurl": "${mapKey},0:${data.longitude},${data.latitude}",
+              "picurl": "${mapKey},0:${longitude},${latitude}",
               "description": "${status} 启动时间 ${GMT}",
               "url": "${mapUrl}"
             }
@@ -460,7 +464,7 @@ F_MGR.readString(cacheFile)
           "articles": [
             {
               "title": "${address}",
-              "picurl": "${mapKey},0:${data.longitude},${data.latitude}",
+              "picurl": "${mapKey},0:${longitude},${latitude}",
               "description": "${status} 更新时间 ${GMT}",
               "url": "${mapUrl}"
             }
