2048f3556e7609a1a5c810ad8cd983c1ee07a843
95度茅台 <dj4qiao@sina.com>
2023-05-19 13:36:38 +0800

    update script

--- table/jingDong_Bill.js
+++ table/jingDong_Bill.js
@@ -206,7 +206,7 @@ async function main() {
       ]
       widget.backgroundGradient = gradient;
     } else {
-      widget.backgroundColor = Color.dynamic(new Color('#FFE5B4', 0.5), new Color('#1e1e1e'));
+      widget.backgroundColor = Color.dynamic(new Color('#fefefe', 0.5), new Color('#1e1e1e'));
     }
     
     
