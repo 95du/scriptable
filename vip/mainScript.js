52803158bb7eab3167812b472ed522bfc06ba552
95度茅台 <dj4qiao@sina.com>
2023-06-20 15:01:39 +0800

    update script

--- vip/mainScript.js
+++ vip/mainScript.js
@@ -1,260 +1,256 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: teal; icon-glyph: cloud-download-alt;
-async function main() {
-  const bgColor = Color.dynamic(
-    new Color('#F5F5F5'),
-    new Color('#000000')
-  );
-  const Files = FileManager.iCloud();
-  const RootPath = Files.documentsDirectory();
-  
-  const saveFileName = (fileName) => {
-    const hasSuffix = fileName.lastIndexOf(".") + 1;
-    return !hasSuffix ? `${fileName}.js` : fileName;
-  };
-  
-  const write = (fileName, content) => {
-    let file = saveFileName(fileName);
-    const filePath = Files.joinPath(RootPath, file);
-    Files.writeString(filePath, content);
-    return true;
-  };
-  
-  const saveFile = async ({ moduleName, url }) => {
-    const req = new Request(url);
-    const content = await req.loadString();
-    write(`${moduleName}`, content);
-    return true;
-  };
-  
-  const notify = async (title, body, url, opts = {}) => {
-    let n = new Notification();
-    n = Object.assign(n, opts);
-    n.title = title;
-    n.body = body;
-    n.sound = 'popup';
-    if (url) n.openURL = url;
-    return await n.schedule();
-  };
-  
-  const renderTableList = async (data) => {
-    try {
-      const table = new UITable();
-      table.showSeparators = true;
-  
-      const gifRow = new UITableRow();
-      gifRow.height = 85 * Device.screenScale();
-      gifRow.backgroundColor = bgColor
-      const gifImage = gifRow.addImageAtURL(atob('aHR0cHM6Ly9zd2VpeGluZmlsZS5oaXNlbnNlLmNvbS9tZWRpYS9NMDAvNzEvQzgvQ2g0RnlXT0k2b0NBZjRQMUFFZ0trSzZxVVVrNTQyLmdpZg=='));
-      gifImage.widthWeight = 0.4;
-      gifImage.centerAligned();
-      table.addRow(gifRow);
-  
-      // topRow
-      const topRow = new UITableRow();
-      topRow.height = 70;
-      const leftText = topRow.addButton('效果图');
-      leftText.widthWeight = 0.3;
-      leftText.onTap = async () => {
-        const webView = new WebView();
-        await webView.loadURL('https://gitcode.net/4qiao/framework/raw/master/img/picture/Example.png');
-        await webView.present(false);
-      };
-  
-      const authorImage = topRow.addImageAtURL('https://gitcode.net/4qiao/framework/raw/master/img/icon/4qiao.png');
-      authorImage.widthWeight = 0.4;
-      authorImage.centerAligned();
-  
-      const rightText = topRow.addButton('快捷指令');
-      rightText.widthWeight = 0.3;
-      rightText.rightAligned();
-      rightText.onTap = async () => {
-        Safari.openInApp('https://sharecuts.cn/user/KVlQooAqzA',false);
-      };
-      table.addRow(topRow);
-  
-      // interval 1
-      await gapRow(table);
-      
-  
-      // 如果是节点，则先远程获取
-      const req = new Request(data.subscription);
-      const subscription = await req.loadJSON();
-      const apps = subscription.apps;
-      apps.forEach((item) => {
-        const r = new UITableRow();
-        r.height = 60;
-        const imgCell = UITableCell.imageAtURL(item.thumb);
-        imgCell.centerAligned();
-        r.addCell(imgCell);
-  
-        const nameCell = UITableCell.text(item.title);
-        nameCell.centerAligned();
-        r.addCell(nameCell);
-  
-        const downloadCell = UITableCell.button("获取");
-        downloadCell.centerAligned();
-        downloadCell.dismissOnTap = true;
-        downloadCell.onTap = async () => {
-          if (item.depend) {
-            try {
-              for (let i = 0; i < item.depend.length; i++) {
-                const relyItem = item.depend[i];
-                const _isWrite = await saveFile({
-                  moduleName: relyItem.name,
-                  url: relyItem.scriptURL,
-                });
-                if (_isWrite) {
-                  notify('', `依赖插件:${relyItem.name}下载/更新成功`);
-                }
-              }
-            } catch (e) {
-              console.log(e);
-            }
-          }
-          const isWrite = await saveFile({
-            moduleName: item.name,
-            url: item.scriptURL,
-          });
-          if (isWrite) {
-            notify('', `小组件:${item.title}下载/更新成功`);
-          }
-        };
-        r.addCell(downloadCell);
-        table.addRow(r);
-      });
-      
-      // interval 2
-      await gapRow(table);
-      
-      // videoRow
-      const videoRow = new UITableRow();
-      videoRow.height = 70;
-      const videoText = videoRow.addButton('Animusic HD Pipe Dreams Video');
-      videoText.widthWeight = 0.3;
-      videoText.centerAligned();
-      videoText.onTap = async () => {
-        await Safari.openInApp(atob('aHR0cHM6Ly9pbWFnZS5mb3N1bmhvbGlkYXkuY29tL2NsL2ltYWdlL2NvbW1lbnQvNjQ4ZGIwNGIwY2NmNmMxZjdlNzFkY2I4X3VwbG9hZC5tcDQ='),false);
-      };
-      table.addRow(videoRow);
-      
-      // bottom interval
-      const bottom = new UITableRow();
-      bottom.height = 225;
-      bottom.backgroundColor = bgColor
-      const bottomText = bottom.addText('Copyright ©️ 2022 界面修改自·@DmYY');
-      bottomText.widthWeight = 0.3;
-      bottomText.centerAligned();
-      bottomText.titleFont = Font.boldMonospacedSystemFont(10);
-      bottomText.titleColor = Color.gray();
-      table.addRow(bottom);
-      table.present(false);
-    } catch (e) {
-      console.log(e);
-      notify("错误提示", "订阅获取失败");
-    }
-  };
-  
-  const Run = async () => {
-    const cacheKey = 'subscriptionList';
-    try {
-      // 默认订阅列表
-      const defaultSubscribeList = [{
-      author: '95度茅台',
-      subscription: 'https://gitcode.net/4qiao/framework/raw/master/scriptable/install.json'
-    }]
-      const mainAlert = new Alert();
-      mainAlert.title = "小组件下载";
-      mainAlert.message = "可自行添加订阅地址";
-      // const cacheKey = "subscriptionList";
-      const render = async () => {
-        let subscriptionList = [];
-        if (Keychain.contains(cacheKey)) {
-          subscriptionList = JSON.parse(Keychain.get(cacheKey));
-        }
-        const _actions = [];
-        subscriptionList.forEach((item) => {
-          const { author } = item;
-          mainAlert.addAction(author);
-          _actions.push(async () => {
-            await renderTableList(item);
-          });
-        });
-  
-        _actions.push(async () => {
-          const a = new Alert();
-          a.title = "输入订阅地址";
-          a.message = '自行替换输入框内的链接'
-          a.addTextField(
-            "URL", 'https://gitcode.net/4qiao/framework/raw/master/scriptable/install.json'
-          );
-          a.addAction("确定");
-          a.addCancelAction("取消");
-          const id = await a.presentAlert();
-          if (id === -1) return;
+// icon-color: brown; icon-glyph: cloud-download-alt;
+const bgColor = Color.dynamic(
+  new Color('#F5F5F5'),
+  new Color('#000000')
+);
+const Files = FileManager.iCloud();
+const RootPath = Files.documentsDirectory();
+
+const saveFileName = (fileName) => {
+  const hasSuffix = fileName.lastIndexOf(".") + 1;
+  return !hasSuffix ? `${fileName}.js` : fileName;
+};
+
+const write = (fileName, content) => {
+  let file = saveFileName(fileName);
+  const filePath = Files.joinPath(RootPath, file);
+  Files.writeString(filePath, content);
+  return true;
+};
+
+const saveFile = async ({ moduleName, url }) => {
+  const req = new Request(encodeURI(url));
+  const content = await req.loadString();
+  write(`${moduleName}`, content);
+  return true;
+};
+
+const notify = async (title, body, url, opts = {}) => {
+  let n = new Notification();
+  n = Object.assign(n, opts);
+  n.title = title;
+  n.body = body;
+  n.sound = 'popup';
+  if (url) n.openURL = url;
+  return await n.schedule();
+};
+
+const renderTableList = async (data) => {
+  try {
+    const table = new UITable();
+    table.showSeparators = true;
+
+    const gifRow = new UITableRow();
+    gifRow.height = 85 * Device.screenScale();
+    gifRow.backgroundColor = bgColor
+    const gifImage = gifRow.addImageAtURL(atob('aHR0cHM6Ly9zd2VpeGluZmlsZS5oaXNlbnNlLmNvbS9tZWRpYS9NMDAvNzEvQzgvQ2g0RnlXT0k2b0NBZjRQMUFFZ0trSzZxVVVrNTQyLmdpZg=='));
+    gifImage.widthWeight = 0.4;
+    gifImage.centerAligned();
+    table.addRow(gifRow);
+
+    // topRow
+    const topRow = new UITableRow();
+    topRow.height = 70;
+    const leftText = topRow.addButton('效果图');
+    leftText.widthWeight = 0.3;
+    leftText.onTap = async () => {
+      const webView = new WebView();
+      await webView.loadURL('https://gitcode.net/4qiao/framework/raw/master/img/picture/Example.png');
+      await webView.present(false);
+    };
+
+    const authorImage = topRow.addImageAtURL('https://gitcode.net/4qiao/framework/raw/master/img/icon/4qiao.png');
+    authorImage.widthWeight = 0.4;
+    authorImage.centerAligned();
+
+    const rightText = topRow.addButton('快捷指令');
+    rightText.widthWeight = 0.3;
+    rightText.rightAligned();
+    rightText.onTap = async () => {
+      Safari.openInApp('https://sharecuts.cn/user/KVlQooAqzA',false);
+    };
+    table.addRow(topRow);
+
+    // interval 1
+    await gapRow(table);
+
+    // 如果是节点，则先远程获取
+    const req = new Request(data.subscription);
+    const subscription = await req.loadJSON();
+    const apps = subscription.apps;
+    apps.forEach((item) => {
+      const r = new UITableRow();
+      r.height = 60;
+      const imgCell = UITableCell.imageAtURL(item.thumb);
+      imgCell.centerAligned();
+      r.addCell(imgCell);
+
+      const nameCell = UITableCell.text(item.title);
+      nameCell.centerAligned();
+      r.addCell(nameCell);
+
+      const downloadCell = UITableCell.button("获取");
+      downloadCell.centerAligned();
+      downloadCell.dismissOnTap = true;
+      downloadCell.onTap = async () => {
+        if (item.depend) {
           try {
-            const url = a.textFieldValue(0);
-            const response = await new Request(url).loadJSON();
-            delete response.apps;
-            const data = [];
-            let isPush = true;
-            for (let i in subscriptionList) {
-              const item = subscriptionList[i];
-              if (response.author === item.author) {
-                isPush = false;
-                data.push({ ...response, subscription: url });
-              } else {
-                data.push(item);
+            for (let i = 0; i < item.depend.length; i++) {
+              const relyItem = item.depend[i];
+              const _isWrite = await saveFile({
+                moduleName: relyItem.name,
+                url: relyItem.scriptURL,
+              });
+              if (_isWrite) {
+                notify('', `依赖插件:${relyItem.name}下载/更新成功`);
               }
             }
-            if (isPush) data.push({ author: response.author, subscription: url });
-            Keychain.set(cacheKey, JSON.stringify(data));
-            await Run();
           } catch (e) {
             console.log(e);
-            notify("错误提示", "订阅地址错误，不是一个 JSON 格式");
           }
+        }
+        const isWrite = await saveFile({
+          moduleName: item.name,
+          url: item.scriptURL,
         });
-        _actions.push(async () => {
-          Keychain.set(cacheKey, JSON.stringify(defaultSubscribeList))
-          await Run();
-      });
-        // Main Menu
-        mainAlert.addAction("添加订阅");
-        mainAlert.addDestructiveAction("重置订阅");
-        mainAlert.addCancelAction("取消操作");
-        const _actionsIndex = await mainAlert.presentSheet();
-        if (_actions[_actionsIndex]) {
-          const func = _actions[_actionsIndex];
-          await func();
+        if (isWrite) {
+          notify('', `小组件:${item.title}下载/更新成功`);
         }
       };
-      await render();
-    } catch (e) {
-      Keychain.remove(cacheKey);
-      console.log("缓存读取错误" + e);
-    }
-  };
-  
-  async function gapRow(table) {
-    const gapRow = new UITableRow();
-    gapRow.height = 30;
-    gapRow.backgroundColor = bgColor
-    return table.addRow(gapRow);
+      r.addCell(downloadCell);
+      table.addRow(r);
+    });
+    
+    // interval 2
+    await gapRow(table);
+    
+    // telegramRow
+    const telegramRow = new UITableRow();
+    telegramRow.height = 70;
+    const telegram = telegramRow.addButton('加入 Scriptable 小组件交流群');
+    telegram.widthWeight = 0.3;
+    telegram.centerAligned();
+    telegram.onTap = async () => {
+      Safari.openInApp('https://t.me/+CpAbO_q_SGo2ZWE1', false);
+    };
+    table.addRow(telegramRow);
+    
+    // bottom interval
+    const bottom = new UITableRow();
+    bottom.height = 225;
+    bottom.backgroundColor = bgColor
+    const bottomText = bottom.addText('Copyright ©️ 2022 界面修改自·@DmYY');
+    bottomText.widthWeight = 0.3;
+    bottomText.centerAligned();
+    bottomText.titleFont = Font.boldMonospacedSystemFont(10);
+    bottomText.titleColor = Color.gray();
+    table.addRow(bottom);
+    table.present(false);
+  } catch (e) {
+    console.log(e);
+    notify("错误提示", "订阅获取失败");
   }
-  
-  (async () => {
-    try {
-      const modules = {
-        moduleName: "widget.Install",
-        url: "https://gitcode.net/4qiao/scriptable/raw/master/api/95duScriptStore.js",
-      };
-      const result = await saveFile(modules);
-    } catch (e) {
-      console.log(e);
-    }
-  })();
-  await Run();
+};
+
+const Run = async () => {
+  const cacheKey = 'subscriptionList'
+  try {
+    // 默认订阅列表
+    const defaultSubscribeList = [{
+    author: '95度茅台',
+    subscription: 'https://gitcode.net/4qiao/framework/raw/master/scriptable/install.json'
+  }]
+    const mainAlert = new Alert();
+    mainAlert.title = "小组件下载";
+    mainAlert.message = "可自行添加订阅地址";
+    // const cacheKey = 'subscriptionList';
+    const render = async () => {
+      let subscriptionList = [];
+      if (Keychain.contains(cacheKey)) {
+        subscriptionList = JSON.parse(Keychain.get(cacheKey));
+      }
+      const _actions = [];
+      subscriptionList.forEach((item) => {
+        const { author } = item;
+        mainAlert.addAction(author);
+        _actions.push(async () => {
+          await renderTableList(item);
+        });
+      });
+
+      _actions.push(async () => {
+        const a = new Alert();
+        a.title = "输入订阅地址";
+        a.addTextField(
+          "URL", 'https://gitcode.net/4qiao/framework/raw/master/scriptable/install.json'
+        );
+        a.addAction("确定");
+        a.addCancelAction("取消");
+        const id = await a.presentAlert();
+        if (id === -1) return;
+        try {
+          const url = a.textFieldValue(0);
+          const response = await new Request(url).loadJSON();
+          delete response.apps;
+          const data = [];
+          let isPush = true;
+          for (let i in subscriptionList) {
+            const item = subscriptionList[i];
+            if (response.author === item.author) {
+              isPush = false;
+              data.push({ ...response, subscription: url });
+            } else {
+              data.push(item);
+            }
+          }
+          if (isPush) data.push({ author: response.author, subscription: url });
+          Keychain.set(cacheKey, JSON.stringify(data));
+          await Run();
+        } catch (e) {
+          console.log(e);
+          notify("错误提示", "订阅地址错误，非 JSON 格式");
+        }
+      });
+      _actions.push(async () => {
+        Keychain.set(cacheKey, JSON.stringify(defaultSubscribeList))
+        await Run();
+    });
+      // Main Menu
+      mainAlert.addAction("添加订阅");
+      mainAlert.addDestructiveAction("重置订阅");
+      mainAlert.addCancelAction("取消操作");
+      const _actionsIndex = await mainAlert.presentSheet();
+      if (_actions[_actionsIndex]) {
+        const func = _actions[_actionsIndex];
+        await func();
+      }
+    };
+    await render();
+  } catch (e) {
+    Keychain.remove(cacheKey);
+    console.log("缓存读取错误" + e);
+  }
+};
+
+async function gapRow(table) {
+  const gapRow = new UITableRow();
+  gapRow.height = 30;
+  gapRow.backgroundColor = bgColor;
+  return table.addRow(gapRow);
 }
-module.exports = { main }
+
+(async () => {
+  try {
+    const modules = {
+      moduleName: "widget.Install",
+      url: "https://gitcode.net/4qiao/scriptable/raw/master/api/95duScriptStore.js",
+    };
+    const result = await saveFile(modules);
+  } catch (e) {
+    console.log(e);
+  }
+})();
+
+await Run();
