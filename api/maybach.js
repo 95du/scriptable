0add06f0accec6b69b83b66a5794530e3011dc4d
95度茅台 <dj4qiao@sina.com>
2023-08-08 02:40:12 +0800

    update script

--- api/maybach.js
+++ api/maybach.js
@@ -193,11 +193,11 @@ async function getRandomImage() {
  */
 const useFileManager = ({ cacheTime } = {}) => {
   return {
-    readImage: (filePath) => {
-      const imgPath = F_MGR.joinPath(cache, filePath);
+    readImage: (fileName) => {
+      const imgPath = F_MGR.joinPath(cache, fileName);
       return F_MGR.fileExists(imgPath) ? F_MGR.readImage(imgPath) : null;
     },
-    writeImage: (filePath, image) => F_MGR.writeImage(F_MGR.joinPath(cache, filePath), image)
+    writeImage: (fileName, image) => F_MGR.writeImage(F_MGR.joinPath(cache, fileName), image)
   }
 };
   
