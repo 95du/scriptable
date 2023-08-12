ecddb0fceef1c37dc84d6798aa16606834cf360b
95度茅台 <dj4qiao@sina.com>
2023-08-13 05:32:28 +0800

    update script

--- table/gps_locating.js
+++ table/gps_locating.js
@@ -37,7 +37,7 @@ async function main() {
    * @param { JSON } string
    */
   const writeSettings = async (inObject) => {
-    F_MGR.writeString(cacheFile, JSON.stringify(inObject), null, 2);
+    F_MGR.writeString(cacheFile, JSON.stringify(inObject, null, 2));
     console.log(JSON.stringify(
       inObject, null, 2
     ));
