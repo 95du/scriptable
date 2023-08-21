feb4a2ab42f1786ebdfe851a630184ebcc03aa5d
95度茅台 <dj4qiao@sina.com>
2023-08-22 01:59:03 +0800

    update script

--- vip/mainScript.js
+++ vip/mainScript.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: purple; icon-glyph: cog;
-
+main()
 async function main() {
   const uri = Script.name();
   const scriptName = 'Script Store'
@@ -1190,7 +1190,7 @@ document.getElementById('telegram').addEventListener('click', () => {
           if (currentChar < message.length) {
             chatMessage.textContent += message[currentChar++];
             chatMessage.scrollTop = chatMessage.scrollHeight;
-            setTimeout(typeNextChar, 150);
+            setTimeout(typeNextChar, 100);
           }
         }
         typeNextChar();
