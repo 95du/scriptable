675540c71f2ec50c386cd598c4d1e29fc48625c9
95度茅台 <dj4qiao@sina.com>
2023-08-28 16:49:31 +0800

    update script

--- table/Oil_Price.js
+++ table/Oil_Price.js
@@ -128,7 +128,7 @@ async function main() {
     barStack1.borderColor = new Color('#D50000', 0.8);
     barStack1.borderWidth = 2.5
     // bar text
-    const oilTipsText = barStack1.addText(forecast);
+    const oilTipsText = barStack1.addText(forecast.length < 45 ? forecast + '，大家互相转告油价调整信息' : forecast);
     oilTipsText.textColor = fm.fileExists(bgImage) ? Color.white() : new Color('#5e5e5e');
     oilTipsText.font = Font.boldSystemFont(13);
     oilTipsText.centerAlignText();
