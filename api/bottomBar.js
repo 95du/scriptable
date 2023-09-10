7020eb8ece04b98fc416e14980a92ac36b7c2fa3
95度茅台 <dj4qiao@sina.com>
2023-09-11 05:08:01 +0800

    update script

--- api/bottomBar.js
+++ api/bottomBar.js
@@ -281,15 +281,20 @@ const downloadModule = async (scriptName, url) => {
 };
 
 const presentMenu = async() => {
-  const menuAlert = new Alert();
-  menuAlert.message = "\n【 iOS 16 负一屏底栏 】\n高仿微信通知信息样式，内容显示未来两小时天气，\n底部显示每日一句中英文";
-  menuAlert.addDestructiveAction('更新代码');
-  menuAlert.addDestructiveAction('重置所有');
-  menuAlert.addAction('透明背景');
-  menuAlert.addAction('组件商店');
-  menuAlert.addAction('预览组件');
-  menuAlert.addCancelAction('取消');
-  const mainMenu = await menuAlert.presentSheet();
+  const alert = new Alert();
+  alert.message = "\n【 iOS 16 负一屏底栏 】\n高仿微信通知信息样式，内容显示未来两小时天气，\n底部显示每日一句中英文";
+  const actions = [
+    '更新代码', '重置所有', '透明背景', '更多组件',  '预览组件'
+  ];
+
+  actions.forEach(( action, index ) => {
+  alert[ index === 0 || index === 1 
+    ? 'addDestructiveAction'
+    : 'addAction' ](action);
+  });
+  alert.addCancelAction('取消');
+  
+  const mainMenu = await alert.presentSheet();
   if (mainMenu === 1) {
     await fm.remove(path);
     const bgImage = await getBgImagePath();
