14a9e63051e7569a2cbf1fb87c4a65deec7274ea
95度茅台 <dj4qiao@sina.com>
2023-10-07 00:52:19 +0800

    update script

--- table/south_PowerGrid.js
+++ table/south_PowerGrid.js
@@ -6,7 +6,7 @@
  * UITable 版本: Version 1.0.0
  * 2023-04-23 19:30
  */
-await 
+
 async function main() {
   const F_MGR = FileManager.local();
   const path = F_MGR.joinPath(F_MGR.documentsDirectory(), '95du_electric');
