1a99426ed0c278b195198c8e5d123f8251fc576a
95度茅台 <dj4qiao@sina.com>
2023-10-12 23:33:09 +0800

    update script

--- table/Oil_Price.js
+++ table/Oil_Price.js
@@ -29,8 +29,7 @@ async function main() {
     
   try {  
     const html = await new Request(atob('aHR0cDovL20ucWl5b3VqaWFnZS5jb20=')).loadString();
-    const str = html.match(/var tishiContent="(.*?)";/)[1].replace("<br/>", ',');
-    forecast = str.match(/([^<]+)<br\/>/)[1];
+    forecast = html.match(/var tishiContent="(.*?)";/)[1].replace("<br/>", ',');
   } catch(e) { 
     console.log(e);
     forecast = setting.oil;
