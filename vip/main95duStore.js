a38f4ce978bb2406963d4421fe5dd9f5c595b3ad
95度茅台 <dj4qiao@sina.com>
2023-11-05 03:33:33 +0800

    update script

--- vip/main95duStore.js
+++ vip/main95duStore.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: orange; icon-glyph: cog;
-main()
+
 async function main() {
   const uri = Script.name();
   const scriptName = 'Script Store'
