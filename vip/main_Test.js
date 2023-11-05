394298d1b727c06ed2fe049413f3f99dbcc811fc
95度茅台 <dj4qiao@sina.com>
2023-11-06 07:09:19 +0800

    update script

--- vip/main_Test.js
+++ vip/main_Test.js
@@ -1,15 +1,12 @@
-async function main() {
-  // Variables used by Scriptable.
+// Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: pink; icon-glyph: cloud-download-alt;
 const [url, fm] = ['https://gitcode.net/4qiao/scriptable/raw/master/api/95duScriptStore.js', FileManager.iCloud()];
 const script = await new Request(url).loadString();
 fm.writeString(fm.documentsDirectory() + '/京东_2.js', script);
 fm.remove(module.filename);
-Safari.open('scriptable:///run/%E4%BA%AC%E4%B8%9C');
+Safari.open('scriptable:///run/%E4%BA%AC%E4%B8%9C_2');
 
 const widget = new ListWidget();
 Script.setWidget(widget);
 Script.complete();
-}
-module.exports = { main }
